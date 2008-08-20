6cb1b6352792b1b979c02cc725d33bd04d2f0551
milipili <milipili@shikami.org>
2008-08-20 15:18:19 +0000

    Various enhancements for the class Yuni::Version
    git-svn-id: file:///home/michael/svn/yuni/trunk@14 7e728022-c0ab-4603-8e65-c666ead28a33

--- src/yuni/misc/version.h
+++ src/yuni/misc/version.h
@@ -21,24 +21,63 @@ namespace Yuni
         //@{
         //! Default constructor
         Version();
+        //! Constructor with a given version
+        Version(const int h, const l, const int r = 0);
         //! Copy constructor
         Version(const Version& c);
         //@}
 
         int hi() const {return pHi;}
-        void hi(const int h) {pHi = h;}
+        void hi(const int h); 
 
         int lo() const {return pLo;}
-        void lo(const int l) {pLo = l;}
+        void lo(const int l);
 
         int revision() const {return pRevision;}
-        void revision(const int r) {pRevision = r;}
+        void revision(const int r);
 
         const String& author() const {return pAuthor;}
-        void author(const String& s) {pAuthor = s;}
+        void author(const String& s);
 
         const String& webUrl() const {return pWebUrl;}
-        void webUrl(const String& s) {pWebUrl = s;}
+        void webUrl(const String& s);
+
+        /*!
+        ** \brief Get the version in an human-readable string
+        */
+        String toString() const;
+
+
+        /*!
+        ** \brief Check if this version is less than another one
+        */
+        bool isLessThan(const Version& rhs) const;
+        /*!
+        ** \brief Check if this version is equal to another one
+        */
+        bool isEqualTo(const Version& rhs) const;
+        /*!
+        ** \brief Check if this version is greater than another one
+        */
+        bool isGreaterThan(const Version& rhs) const;
+
+
+        //! \name Operators
+        //@{
+        //! The operator <
+        bool operator <  (const Version& rhs) const {return isLessThan(rhs);}
+        //! The operator <=
+        bool operator <= (const Version& rhs) const {return isEqualTo(rhs) || isLessThan(rhs);}
+        //! The operator >
+        bool operator >  (const Version& rhs) const {return isGreaterThan(rhs);}
+        //! The operator <=
+        bool operator >= (const Version& rhs) const {return isEqualTo(rhs) || isGreaterThan(rhs);}
+
+        //! The operator ==
+        bool operator == (const Version& rhs) const {return isEqualTo(rhs);}
+        //! The operator !=
+        bool operator != (const Version& rhs) const {return !isEqualTo(rhs);}
+        //@}
 
     private:
         //! The hi version
