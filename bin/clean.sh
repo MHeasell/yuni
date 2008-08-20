68782a5692447915e9c5cdd56a5ee476def8f94a
milipili <milipili@shikami.org>
2008-08-20 11:41:30 +0000

    Added a script to make a complete clean up of the local working copy of the SVN repository
    git-svn-id: file:///home/michael/svn/yuni/trunk@5 7e728022-c0ab-4603-8e65-c666ead28a33

---
+++ bin/clean.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+root=`dirname "${0}"`
+
+find "${root}/../" '(' -name 'cmake*' \
+            -or -name 'CMakeCache*' \
+            -or -name 'CMakeFiles' \
+            -or -name 'Makefile' \
+            -or -name '.*.swp' ')' -exec echo ' * Removing `' '{}' '`' \; -exec rm -rf '{}' \;
