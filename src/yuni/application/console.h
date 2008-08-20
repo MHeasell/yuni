af922b702bde769aab9219aa36ce89e30205894c
milipili <milipili@shikami.org>
2008-08-20 14:33:22 +0000

    Various fixes for the class Yuni::Application::Console and Yuni::Application::Abstract
    git-svn-id: file:///home/michael/svn/yuni/trunk@12 7e728022-c0ab-4603-8e65-c666ead28a33

--- src/yuni/application/console.h
+++ src/yuni/application/console.h
@@ -10,17 +10,57 @@ namespace Yuni
 namespace Application
 {
 
+
+    /*! \class Yuni::Application::Console
+    **
+    ** \brief Console Application
+    **
+    ** \code
+    ** class HelloWorld
+    ** {
+    ** public:
+    **      HelloWorld(nt argc, char* argv[]) : Yuni::Application::Console(argc, argv) {}
+    **      ~HelloWorld() {}
+    **
+    **      virtual void execute()
+    **      {
+    **          std::cout << "Hello world" << std::endl;
+    **      }
+    ** };
+    **
+    ** int main(int argc, char* argv[])
+    ** {
+    **      HelloWorld app(argc, argv);
+    **      app.execute();
+    **      return app.exitCode();
+    ** }
+    ** \endcode
+    */
     class Console : public Application::Abstract
     {
     public:
         /*!
         ** \brief Get the global instance of the console application
         */
-        static Console& Instance();
+        static Console* Instance() {return dynamic_cast<Console*>(Abstract::Instance());}
 
     public:
+        //! \name Constructor & Destructor
+        //@{
+        /*!
+        ** \brief Constructor
+        ** \param argc The number of arguments
+        ** \param argv All arguments
+        */
         Console(int argc, char* argv[]);
+        //! Destructor
         virtual ~Console();
+        //@}
+
+        /*!
+        ** \brief The main routine for the console application
+        */
+        virtual void execute() = 0;
 
     }; // class Application::Console
 
