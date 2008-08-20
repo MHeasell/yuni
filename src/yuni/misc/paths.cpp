ed9e66abdf7973a206587c2d31b497cb9d1553d4
milipili <milipili@shikami.org>
2008-08-20 14:32:31 +0000

    Added a toolbox to handle paths
    git-svn-id: http://svn.ta3d.org/yuni/trunk@11 7e728022-c0ab-4603-8e65-c666ead28a33

---
+++ src/yuni/misc/paths.cpp
@@ -0,0 +1,166 @@
+
+#include <yuni/paths.h>
+#ifndef YUNI_OS_WINDOWS
+# include <stdlib.h>
+#else
+# include <windows.h>
+#endif 
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fstream>
+
+
+
+
+namespace Yuni
+{
+namespace Paths
+{
+    
+
+
+
+    bool IsAbsolute(const String& p)
+    {
+        # ifdef YUNI_OS_WINDOWS
+        return (p.empty() || (p.size() > 2 && ':' == p[1] && '\\' == p[2]));
+        # else
+        return (p.empty() || '/' == p[0]);
+        # endif
+    }
+    
+
+    String CurrentDirectory()
+    {
+        char* c = getcwd(NULL, 0);
+        String ret(c);
+        free(c);
+        return String(ret);
+    }
+
+
+    String ExtractFilePath(const String& p, const bool systemDependant)
+    {
+        String::Vector parts;
+        if (systemDependant)
+            p.split(parts, SeparatorAsString, false);
+        else
+            p.split(parts, "\\/", false);
+        
+        String ret;
+        String::Vector::size_type n = parts.size();
+        --n;
+        for (String::Vector::size_type i = 0; i != n; ++i)
+        {
+            if (parts[i] != ".")
+                ret << parts[i] << Separator;
+        }
+        return ret;
+    }
+
+    String ExtractFileName(const String& p, const bool systemDependant)
+    {
+        String::size_type pos;
+        if (systemDependant)
+            pos = p.find_last_of(Separator);
+        else
+            pos = p.find_last_of("\\/");
+        if (String::npos == pos)
+            return p;
+        return p.substr(pos + 1);
+    }
+
+    void ExtractFileName(String::List& p, const bool systemDependant)
+    {
+        for (String::List::iterator i = p.begin(); i != p.end(); i++)
+            *i = ExtractFileName( *i, systemDependant );
+    }
+
+    void ExtractFileName(String::Vector& p, const bool systemDependant)
+    {
+        for (String::Vector::iterator i = p.begin(); i != p.end(); i++)
+            *i = ExtractFileName( *i, systemDependant );
+    }
+
+    String ExtractFileNameWithoutExtension(const String& p)
+    {
+        String::size_type pos = p.find_last_of(Separator);
+        String::size_type n = p.find_last_of('.');
+        if (String::npos == n && String::npos == pos)
+            return p;
+        if (n == pos)
+            return "";
+        if (n == String::npos && n > pos + 1)
+        {
+            if (String::npos == pos)
+                return p;
+            return p.substr(pos + 1);
+        }
+        if (pos == String::npos)
+            return p.substr(0, n);
+        return p.substr(pos + 1, n - pos - 1);
+    }
+
+
+
+    String ExtractFileExt(const String& s)
+    {
+        String::size_type n = s.find_last_of(".\\/");
+        if (n == String::npos || '.' != s[n])
+            return "";
+        return String(s, n).toLower();
+    }
+
+
+    bool Exists(const String& p)
+    {
+        if (p.empty())
+            return false;
+	    # ifdef YUNI_OS_WINDOWS
+	    // ugly workaround with stat under Windows
+	    // FIXME: Find a better way to find driver letters
+	    if (p.size() == 2 && ':' == p[1]) 
+	        return true;
+	    # endif
+        struct stat s;
+        return (stat(p.c_str(), &s) == 0);
+    }
+
+    bool MakeDir(const String& p)
+    {
+        if (p.empty())
+            return true;
+        String::Vector parts;
+        p.split(parts, SeparatorAsString, false);
+        String pth;
+
+        for (String::Vector::const_iterator i = parts.begin(); i != parts.end(); ++i)
+        {
+            pth += *i;
+	        # ifndef YUNI_OS_WINDOWS
+            pth += Separator;
+            # endif
+            if (!Exists(pth))
+            {
+		        # ifdef YUNI_OS_WINDOWS
+                if (mkdir(pth.c_str()))
+		        # else
+                if (mkdir(pth.c_str(), 01755))
+		        # endif
+                {
+                    // Impossible to create the folder
+                    return false;
+                }
+            }
+	        # ifdef YUNI_OS_WINDOWS
+            pth += Separator;
+	        # endif
+        }
+        return true;
+    }
+
+
+
+} // namespace Paths
+} // namespace TA3D
+
---
+++ src/yuni/misc/paths.h
@@ -0,0 +1,135 @@
+#ifndef __YUNI_MISC_PATHS_H__
+# define __YUNI_MISC_PATHS_H__
+
+# include <yuni/yuni.h>
+# include <yuni/string.h>
+
+
+
+namespace Yuni
+{
+
+/*
+** \brief Path manipulation functions
+*/
+namespace Paths
+{
+   
+
+    //! \name System-dependant variables
+    //@{
+
+    //! The path-separator character according to the platform
+    # ifdef YUNI_OS_WINDOWS
+    const char Separator = '\\';
+    # else
+    const char Separator = '/';
+    # endif
+
+    //! The path-separator character according to the platform (stored in a string instead of a char)
+    # ifdef YUNI_OS_WINDOWS
+    const String SeparatorAsString = "\\";
+    # else
+    const String SeparatorAsString = "/";
+    # endif
+
+    //@}
+
+
+
+
+
+    //! \name Folders handling
+    //@{
+
+    /*!
+    ** \brief Test if a file/folder exists
+    ** \param p The folder/filename to test
+    ** \return True if it exists, false otherwise
+    */
+    bool Exists(const String& p);
+
+    /*!
+    ** \brief Create Path Recursively
+    **
+    ** \param p The path to create if it does not exist
+    ** return True if the operation succeeded, false otherwise
+    */
+    bool MakeDir(const String& p);
+
+    /*!
+    ** \brief Retrieve the current directory
+    */
+    String CurrentDirectory();
+
+    /*!
+    ** \brief Extract the path part of a filename
+    **
+    ** The path part will be extracted according the system-dependant path-separator
+    **
+    ** \param systemDependant Consider only the system-dependant path-separator
+    **
+    ** \code
+    **      std::cout << Paths::ExtractFilePath("/tmp/foo.txt") std::endl; // write `/tmp/`
+    **      std::cout << Paths::ExtractFilePath("/tmp/") std::endl; // write `/tmp/`
+    **      std::cout << Paths::ExtractFilePath("/tmp") std::endl; // write `/`
+    ** \endcode
+    **
+    ** \see Paths::Separator
+    */
+    String ExtractFilePath(const String& p, const bool systemDependant = true);
+
+    /*!
+    ** \brief Extract the bare file name
+    **
+    ** The file name will be extracted according the last occurence
+    ** of the system-dependant path-separator (if systemDependant = true)
+    **
+    ** \param systemDependant Consider only the system-dependant path-separator
+    **
+    ** \see Paths::Separator
+    */
+    String ExtractFileName(const String& p, const bool systemDependant = true);
+    void ExtractFileName(String::List& p, const bool systemDependant = true);
+    void ExtractFileName(String::Vector& p, const bool systemDependant = true);
+
+    /*!
+    ** \brief Extract the bare file name without its extension
+    **
+    ** The file name will be extracted according the last occurence
+    ** of the system-dependant path-separator
+    **
+    ** \see Paths::Separator
+    */
+    String ExtractFileNameWithoutExtension(const String& p);
+
+    /*!
+    ** \brief Extract the extention of a file name
+    ** \param s Filename
+    ** \return The extenion of the filename (with the leading '.') in lowercase, empty if no extension is present
+    **
+    ** \code
+    **     std::cout << Paths::Files::ExtractFileExt("foo.exe") << std::endl; // '.exe'
+    **     std::cout << Paths::Files::ExtractFileExt("/usr/folder.foo/file") << std::endl; // ''
+    ** \endcode
+    */
+    String ExtractFileExt(const String& s);
+
+
+    /*!
+    ** \brief Get if a path is absolute 
+    **
+    ** \param p The path or the filename to test
+    ** \return True if the path is an absolute path or empty, false otherwise
+    */
+    bool IsAbsolute(const String& p);
+
+    //@}
+
+
+
+} // namespace Paths
+} // namespace Yuni
+
+
+#endif // __YUNI_MISC_PATHS_H__
