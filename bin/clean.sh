5396d93a987ffcb3d720d82c63000c12613926a9
milipili <milipili@shikami.org>
2008-08-20 11:42:31 +0000

    Added a missing pattern to remove
    git-svn-id: file:///home/michael/svn/yuni/trunk@6 7e728022-c0ab-4603-8e65-c666ead28a33

--- bin/clean.sh
+++ bin/clean.sh
@@ -6,4 +6,5 @@ find "${root}/../" '(' -name 'cmake*' \
             -or -name 'CMakeCache*' \
             -or -name 'CMakeFiles' \
             -or -name 'Makefile' \
+            -or -name '*.o' \
             -or -name '.*.swp' ')' -exec echo ' * Removing `' '{}' '`' \; -exec rm -rf '{}' \;
