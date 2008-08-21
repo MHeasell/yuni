79357adbc7bc7df0bba63cdf675f09361ff14d08
milipili <milipili@shikami.org>
2008-08-21 13:01:59 +0000

    Added missing files
    git-svn-id: http://svn.ta3d.org/yuni/trunk@21 7e728022-c0ab-4603-8e65-c666ead28a33

---
+++ src/samples/sharedptr/00.sharedptr/main.cpp
@@ -0,0 +1,21 @@
+
+#include <yuni/yuni.h>
+#include <yuni/sharedptr.h>
+#include <iostream>
+
+struct Foo
+{
+    Foo()  {std::cout << "Constructor" << std::endl;}
+    ~Foo() {std::cout << "Destructor" << std::endl;}
+};
+
+int main(void)
+{
+    // We create an instance of Foo
+    Yuni::SharedPtr<Foo> p (new Foo());
+    Yuni::SharedPtr<Foo> t; // NULL by default
+
+    // Assignation. `t` and `p` will share the same pointer
+    t = p;
+    return 0;
+}
