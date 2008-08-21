2d1e854ffcda0f581faea6a759562af85cada5f5
milipili <milipili@shikami.org>
2008-08-21 15:51:15 +0000

    Added a class template specialization for the class Yuni::Hash::Dictionary<String>
    git-svn-id: http://svn.ta3d.org/yuni/trunk@23 7e728022-c0ab-4603-8e65-c666ead28a33

--- src/yuni/hash/dictionary.h
+++ src/yuni/hash/dictionary.h
@@ -3,6 +3,7 @@
 
 # include <yuni/yuni.h>
 # include <yuni/hash/table.h>
+# include <yuni/string.h>
 
 
 namespace Yuni
@@ -52,9 +53,57 @@ namespace Hash
     }; // class Hash::Dictionary
 
 
+    template<>
+    class Dictionary<String> : public Table<String, String>
+    {
+    public:
+        //! \name Constructors & Destructor
+        //@{
+        //! Default constructor
+        Dictionary() : Table<String, String>() {}
+        //! Copy constructor
+        Dictionary(const Dictionary& c) : Table<String, String>(c) {}
+        //! Destructor
+        virtual ~Dictionary() {Table<String, String>::clear();}
+        //@}
+
+        //! Read the value of a key as a string
+        String toString(const String& key, const String& defvalue = "") const;
+
+        //! Read the value of a key as a string
+        bool toBool(const String& key, const bool defvalue = false) const;
+
+        //! Read the value of a key as a string
+        float toFloat(const String& key, const float defvalue = 0.0f) const;
+        //! Read the value of a key as a string
+        double toDouble(const String& key, const double defvalue = 0.0f) const;
+
+
+        //! Read the value of a key as a signed int 8 bits
+        sint8 toInt8(const String& key, const sint8 defvalue = 0) const;
+        //! Read the value of a key as a signed int 16 bits
+        sint16 toInt16(const String& key, const sint16 defvalue = 0) const;
+        //! Read the value of a key as a signed int 32 bits
+        sint32 toInt32(const String& key, const sint32 defvalue = 0) const;
+        //! Read the value of a key as a signed int 64 bits
+        sint64 toInt64(const String& key, const sint64 defvalue = 0) const;
+        //! Read the value of a key as a unsigned int 8 bits
+        uint8 toUInt8(const String& key, const uint8 defvalue = 0) const;
+        //! Read the value of a key as a unsigned int 16 bits
+        uint16 toUInt16(const String& key, const uint16 defvalue = 0) const;
+        //! Read the value of a key as a unsigned int 32 bits
+        uint32 toUInt32(const String& key, const uint32 defvalue = 0) const;
+        //! Read the value of a key as a unsigned int 64 bits
+        uint64 toUInt64(const String& key, const uint64 defvalue = 0) const;
+
+    }; // class Hash::Dictionary<String>
+
 
 } // namespace Hash
 } // namespace Yuni
+
+
+# include <yuni/hash/dictionary.hxx>
 
 #endif // __YUNI_HASH_DICTIONARY_H__
 
