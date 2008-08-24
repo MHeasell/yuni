aab56a03b3dc18187bf7892fe77fc3a8e53d4953
milipili <milipili@shikami.org>
2008-08-24 22:07:47 +0000

    Started the implementation for the user interface
    git-svn-id: http://svn.ta3d.org/yuni/trunk@49 7e728022-c0ab-4603-8e65-c666ead28a33

---
+++ src/yuni/gfx/ui/component.cpp
@@ -0,0 +1,98 @@
+
+# include <yuni/yuni.h>
+# include <yuni/gfx/point2d.h>
+# include <yuni/gfx/ui/component.h>
+
+
+namespace Yuni
+{
+namespace Gfx
+{
+namespace UI
+{
+
+    Component::Component(Window& wnd, Component* o)
+        :pWindow(wnd), pOwner(o)
+    {
+        if (o)
+            o->items.add(this);
+    }
+
+    Component::~Component()
+    {
+        items.clear();
+        if (pOwner)
+            pOwner->items.remove(this);
+        pWindow.unregisterComponent(this);
+    }
+
+    void Component::name(const String& n)
+    {
+        pWindow.unregisterComponent(this);
+        pName = n;
+        pWindow.registerComponent(this);
+    }
+
+
+
+    Component::Items::Items()
+        :pList()
+    {}
+
+    Component::Items::~Items()
+    {
+        // clear();  Already done by Component::~Component
+    }
+
+
+    void Component::Items::add(Component* c)
+    {
+        if (c && !this->exists(c))
+            pList.push_back(c);
+    }
+
+    void Component::Items::remove(Component* c)
+    {
+        if (c)
+        {
+            for (List::iterator i = pList.begin(); i != pList.end(); ++i)
+            {
+                if (c == *i)
+                {
+                    delete *i;
+                    pList.erase(i);
+                    return;
+                }
+            }
+        }
+    }
+
+
+    bool Component::Items::exists(Component* c)
+    {
+        if (c)
+        {
+            for (List::const_iterator i = pList.begin(); i != pList.end(); ++i)
+            {
+                if (c == *i)
+                    return true;
+            }
+        }
+        return false;
+    }
+
+
+    void Component::Items::clear()
+    {
+        for (List::iterator i = pList.begin(); i != pList.end(); ++i)
+            delete *i;
+        pList.clear();
+    }
+
+
+
+} // namespace UI
+} // namespace Gfx
+} // namespace Yuni
+
+
---
+++ src/yuni/gfx/ui/component.h
@@ -0,0 +1,124 @@
+#ifndef __YUNI_USER_INTERFACE_COMPONENT_H__
+# define __YUNI_USER_INTERFACE_COMPONENT_H__
+
+# include <yuni/yuni.h>
+# include <yuni/gfx/point2d.h>
+# include <vector>
+# include <yuni/gfx/ui/window.h>
+# include <yuni/string.h>
+
+
+
+namespace Yuni
+{
+namespace Gfx
+{
+namespace UI
+{
+
+    class Window;
+
+    class Component
+    {
+    public:
+        /*!
+        ** \brief Component List
+        */
+        class Items
+        {
+        public:
+            //! \name Constructor & Destructor
+            //@{
+            //! Default constructor
+            Items();
+            //! Destructor
+            ~Items();
+            //@}
+
+            /*!
+            ** \brief Add a component in the list
+            */
+            void add(Component* c);
+
+            /*!
+            ** \brief Remove
+            */
+            void remove(Component* c);
+
+            /*!
+            ** \brief Clear all sub components
+            */
+            void clear();
+
+            /*!
+            ** \brief Get if a component is already in the list
+            */
+            bool exists(Component* c);
+
+            /*!
+            ** \brief Get the size of the list
+            */
+            uint32 size() const {return pList.size();}
+
+        private:
+            typedef std::vector<Component*> List;
+            List pList;
+
+        }; // class Items
+
+
+    public:
+        //! \name Constructor & Destructor
+        //@{
+        //! \name Constructor & Destructor
+        /*!
+        ** \brief Constructor
+        **
+        ** \param o
+        */
+        Component(Window& wnd, Component* o);
+        //! Destructor
+        virtual ~Component();
+        //@}
+
+        //! The owner of this component
+        Component* owner() const {return pOwner;}
+
+        //! The position of the component
+        const Point2D<int>& position() const {return pPosition;}
+        Point2D<int>& position() {return pPosition;}
+
+        //! The dimension
+        const Point2D<int> size() const {return pSize;}
+        Point2D<int> size() {return pSize;}
+
+        //! Name of the component
+        const String& name() const {return pName;}
+        void name(const String& n);
+
+        //! All sub-components
+        Items items;
+
+
+    protected:
+        //! The parent window
+        Window& pWindow;
+        //! The owner of this component
+        Component* pOwner;
+        //! Name of the component
+        String pName;
+        //! Position
+        Point2D<int> pPosition;
+        //! Size
+        Point2D<int> pSize;
+
+    }; // class Component
+
+
+
+
+} // namespace UI
+} // namespace Gfx
+} // namespace Yuni
+
+#endif // __YUNI_USER_INTERFACE_COMPONENT_H__
