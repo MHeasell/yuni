d1734d5427cbf7a5ba01c8f8b470fb8bd4c53ee7
milipili <milipili@shikami.org>
2008-08-25 14:25:10 +0000

    Math: Added routines for Bound limit checking
    git-svn-id: file:///home/michael/svn/yuni/trunk@53 7e728022-c0ab-4603-8e65-c666ead28a33

--- src/yuni/misc/math.h
+++ src/yuni/misc/math.h
@@ -17,6 +17,36 @@ namespace Math
     float Abs(const float a) {return fabsf(a);}
     double Abs(const double a) {return fabs(a);}
 
+    /*!
+    ** \brief Get the expression considered as the greater
+    **
+    ** \param a The first expression
+    ** \param b The second expression
+    ** \return The expression considered as the grater
+    */
+    template<typename T>
+    T& Max(T& a, T& b) {return a < b ? b : a;}
+
+    /*!
+    ** \brief Get the expression considered as the smaller
+    **
+    ** \param a The first expression
+    ** \param b The second expression
+    ** \return The expression considered as the smaller
+    */
+    T& Min(T& a, T& b) {return a < b ? a : b;}
+
+
+    /*!
+    ** \brief Ensure that an expression was contained in a range of values
+    **
+    ** \param v An expression
+    ** \param min The lower bound limit allowed for the expression `v`
+    ** \param min The upper bound limit allowed for the expression `v`
+    ** \return The expression itself, or a bound limit
+    */
+    T& MinMax(T& v, T& min, T& max) {return ((v < min) ? min : ((v > max) ? max : v));}
+
 
 } // namespace Math
 } // namespace Yuni
