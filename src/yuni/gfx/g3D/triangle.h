7596f45cc8b3bfa4eea4873ba3a9453899f91ea1
loomchild <loomchild@7e728022-c0ab-4603-8e65-c666ead28a33>
2008-08-28 16:34:55 +0000

    Small additions to vertex. Added triangle class.
    git-svn-id: http://svn.ta3d.org/yuni/trunk@68 7e728022-c0ab-4603-8e65-c666ead28a33

---
+++ src/yuni/gfx/g3D/triangle.h
@@ -0,0 +1,58 @@
+
+#ifndef __YUNI_GFX_G3D_TRIANGLE_H__
+# define __YUNI_GFX_G3D_TRIANGLE_H__
+
+# include "edge.h"
+
+namespace Yuni
+{
+namespace Gfx
+{
+namespace G3D
+{
+
+	/*!
+	** \brief A triangle is a 3-sided polygon
+	** The triangle is the base element for all meshes
+	** It knows its edges, and its vertices.
+	*/
+	class Triangle
+	{
+	public:
+		//! \name Constructors
+        //@{
+		Triangle(const Vertex& v1, const Vertex& v2, const Vertex& v3):
+			pVertex1(v1), pVertex2(v2), pVertex3(v3)
+		{}
+		//! \brief Copy constructor
+		Triangle(const Triangle& t):
+			pVertex1(t.Vertex1), pVertex2(t.vertex2), pVertex3(t.Vertex3)
+		{}
+		//@}
+
+		//! \name Accessors
+		//@{
+		const Vertex& Vertex1() const { return pVertex1; }
+		const Vertex& Vertex2() const { return pVertex2; }
+		const Vertex& Vertex3() const { return pVertex3; }
+		//@}
+
+	private:
+		//! \brief Vertices
+		Vertex pVertex1;
+		Vertex pVertex2;
+		Vertex pVertex3;
+
+		//! \brief Edges
+		// TODO
+
+		//! \brief Surface normal
+		Vector3D<float> pNormal;
+	};
+
+
+} // G3D
+} // Gfx
+} // Yuni
+
+#endif // __YUNI_GFX_G3D_TRIANGLE_H__
