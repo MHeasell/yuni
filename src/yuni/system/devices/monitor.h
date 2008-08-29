d27270463620ef86bddd93a189a4d4bc472f199f
milipili <milipili@shikami.org>
2008-08-29 20:02:28 +0000

    Devices: Monitor: Fixed compilation
    git-svn-id: file:///home/michael/svn/yuni/trunk@91 7e728022-c0ab-4603-8e65-c666ead28a33

--- src/yuni/system/devices/monitor.h
+++ src/yuni/system/devices/monitor.h
@@ -2,8 +2,9 @@
 # define __YUNI_SYSTEM_SCREEN_H__
 
 # include <vector>
-# include "../yuni.h"
-# include "../misc/string.h"
+# include "../../yuni.h"
+# include "../../string.h"
+
 
 
 namespace Yuni
@@ -16,6 +17,8 @@ namespace Devices
 
 	/*!
 	** \brief Information about a single monitor
+	**
+	** This class is not thread-safe
 	*/
 	class Monitor
 	{
@@ -25,28 +28,40 @@ namespace Devices
 		//! Value for an invalid handle
 		static const Handle InvalidHandle = (Handle)(-1);
 
+
+
 		/*!
 		** \brief Screen/Monitor resolution
 		*/
-		struct Resolution
+		class Resolution
 		{
+		public:
+			//! Vector of resolutions
+			typedef std::vector<Resolution>  Vector;
+
+		public:
 			//! \name Constructors
 			//@{
 			//! Default constructors
-			Resolution() : width(640), height(480) { /* Default valid values */ }
+			Resolution() : pWidth(640), pHeight(480) {} // Default valid values
 			//! Constructor by copy
-			Resolution(const Resolution& c) : width(c.width), height(c.height) {}
+			Resolution(const Resolution& c) : pWidth(c.pWidth), pHeight(c.pHeight) {}
 			//@}
 
 			//! The width of the monitor/screen
-			uint16 width;
+			uint16 width() const {return pWidth;}
 			//! The height of the monitor/screen
-			uint16 height;
+			uint16 height() const {return pHeight;}
+
+		private:
+			//! Height of the screen
+			uint16 pWidth;
+			//! Width of the screen
+			uint16 pHeight;
 
 		}; // class Resolution
 
-		//! List of monitor resolutions
-		typedef std::vector<Resolution>  ResolutionList;
+
 
 	public:
 		//! \name Constructors and Destructor
@@ -54,7 +69,7 @@ namespace Devices
 		//! Default constructor
 		Monitor() : pHandle(Monitor::InvalidHandle) {}
 		//! Constructor with a given handle
-		explicit Monitor(const Monitor::Handle h) { select(h); }
+		explicit Monitor(const Monitor::Handle h) :pHandle(h) {}
 		//! Destructor
 		~Monitor();
 		//@}
@@ -66,46 +81,20 @@ namespace Devices
 		/*!
 		** \brief Get if the handle of this monitor is valid
 		*/
-		bool isValid() const {return pHandle != Monitor::InvalidHandle; }
-
-		/*!
-		** \brief Select a monitor from its index
-		**
-		** If the given handle is invalid, the handle will be set to `Monitor::InvalidHandle`
-		** and this method will return `false`.
-		**
-		** \param indx The handle of the monitor
-		** \return True if the operation has succeeded, False otherwise
-		*/
-		bool select(const Monitor::Handle indx);
-
-		/*!
-		** \brief Get the current resolution of the selected monitor
-		**
-		** \param[out] res The resolution of the monitor
-		** \return True if the operation has succeeded, False otherwise
-		*/
-		bool currentResolution(Resolution& res) const;
-
-		/*!
-		** \brief Get all supported resolutions by the monitor
-		**
-		** \param[out] list List of all resolutions
-		** \return True if the operation has succeeded, False otherwise
-		*/
-		bool getAllResolutions(ResolutionList& list) const;
+		bool isValid() const {return pHandle != Monitor::InvalidHandle;}
 
 		/*!
 		** \brief Get the name of the monitor, if any
 		**
 		** \return The name of the monitor, or an empty value if not available
 		*/
-		String name() const;
+		String name() const {return pName;}
 
 		/*!
 		** \brief Get an unique id for this screen
 		**
 		** This ID should be considered as unique only on the local computer.
+		** This guid may be useful to load/save settings according the current monitor
 		**
 		** \return A md5 string
 		*/
@@ -114,6 +103,10 @@ namespace Devices
 	private:
 		//! The index of the monitor
 		Monitor::Handle pHandle;
+		//! Name of the current
+		String pName;
+		//! All resolutions
+		Resolution::Vector pResolutions;
 
 	}; // class Monitor
 
