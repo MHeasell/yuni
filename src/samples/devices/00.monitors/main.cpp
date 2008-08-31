6ae31ed415e4304164ef12faec8294c0f3d61e60
milipili <milipili@shikami.org>
2008-08-31 10:36:20 +0000

    Devices: Display: Added a sample program to list all available displays
    git-svn-id: http://svn.ta3d.org/yuni/trunk@94 7e728022-c0ab-4603-8e65-c666ead28a33

---
+++ src/samples/devices/00.monitors/main.cpp
@@ -0,0 +1,59 @@
+
+#include <yuni/yuni.h>
+#include <yuni/sharedptr.h>
+#include <yuni/system/devices/display.h>
+#include <iostream>
+
+
+using namespace Yuni;
+
+
+void title(const String& t)
+{
+	std::cout << std::endl << "--- " << t << " ---" << std::endl << std::endl;
+}
+
+
+void printMonitor(SharedPtr<System::Devices::Display::Monitor> monitor)
+{
+	// The name of the monitor
+	std::cout << "Name: " << (monitor->name().empty() ? "<Unknown>" : monitor->name());
+
+	// Its GUID
+	std::cout << ", guid:" << monitor->guid();
+	if (monitor->primary())
+		std::cout << ", primary";
+	if (monitor->hardwareAcceleration())
+		std::cout << ", Hardware-Accelerated";
+	std::cout << std::endl;
+
+	// All resolutions
+	System::Devices::Display::Resolution::Vector::const_iterator it;
+	for (it = monitor->resolutions().begin(); it != monitor->resolutions().end(); ++it)
+	{
+		// SharedPtr<System::Devices::Display::Resolution> resolution(*it);
+		std::cout << "  . " << (*it)->toString() << std::endl;
+	}
+}
+
+
+
+int main(void)
+{
+	System::Devices::Display::List allDisplays;
+
+	// The primary monitor
+	title("The primary display");
+	printMonitor(allDisplays.primary());
+
+	// All available monitors
+	title("All available displays, the primary display included");
+	System::Devices::Display::Monitor::Vector::const_iterator it;
+	for (it = allDisplays.monitors().begin(); it != allDisplays.monitors().end(); ++it)
+	{
+		printMonitor(*it);
+	}
+
+	std::cout << std::endl;
+	return 0;
+}
