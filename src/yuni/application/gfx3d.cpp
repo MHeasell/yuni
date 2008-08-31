a0ede2a891e1dd45b6ad4a1783d90eb9dddef12f
milipili <milipili@shikami.org>
2008-08-31 20:50:08 +0000

    Application: Gfx3D: Started the implementation with Irrlicht as the default 3D framework backend
    git-svn-id: http://svn.ta3d.org/yuni/trunk@109 7e728022-c0ab-4603-8e65-c666ead28a33

---
+++ src/yuni/application/gfx3d.cpp
@@ -0,0 +1,21 @@
+
+#include "gfx3d.h"
+
+
+namespace Yuni
+{
+namespace Application
+{
+
+	Gfx3D::Gfx3D(int argc, char* argv[])
+		:Abstract(argc, argv), device()
+	{}
+
+	Gfx3D::~Gfx3D()
+	{}
+
+
+} // namespace Application
+} // namespace Yuni
+
+
