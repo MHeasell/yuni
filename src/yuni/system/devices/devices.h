f2379899e9d39ecefbfadf7a1beebe3f8b87a11e
milipili <milipili@shikami.org>
2008-08-31 10:41:07 +0000

    Added some missing files...
    git-svn-id: file:///home/michael/svn/yuni/trunk@95 7e728022-c0ab-4603-8e65-c666ead28a33

---
+++ src/yuni/system/devices/devices.h
@@ -0,0 +1,24 @@
+#ifndef __YUNI_SYSTEM_DEVICES_H__
+# define __YUNI_SYSTEM_DEVICES_H__
+
+
+
+namespace Yuni
+{
+namespace System
+{
+
+/*!
+** \brief Dynamic device handling
+*/
+namespace Devices
+{
+} // namespace Devices
+} // namespace System
+} // namespace Yuni
+
+
+# include "display.h"
+
+
+#endif // __YUNI_SYSTEM_DEVICES_H__
