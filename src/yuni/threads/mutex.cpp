9e44ca4de2f879cf1e5c8cde3adab9bbf8fc8613
milipili <milipili@shikami.org>
2008-09-01 14:32:14 +0000

    Threads: Mutex:  * Fixed bad includes  * A mutex can now be recursive  * Added a class-level locking mechanism
    git-svn-id: file:///home/michael/svn/yuni/trunk@112 7e728022-c0ab-4603-8e65-c666ead28a33

--- src/yuni/threads/mutex.cpp
+++ src/yuni/threads/mutex.cpp
@@ -1,27 +1,33 @@
 
-#include <yuni/yuni.h>
-#include <yuni/threads/mutex.h>
+#include "mutex.h"
 
 
 namespace Yuni
 {
 
 
-	Mutex::Mutex()
+
+	Mutex::Mutex(const bool recursive)
 	{
-		//pthread_mutexattr_t mutexattr;
-		/*
-		pthread_mutexattr_init(&mutexattr);
-		# ifdef YUNI_OS_DARWIN
-		pthread_mutexattr_settype(&mutexattr, PTHREAD_MUTEX_RECURSIVE);
-		# else
-		pthread_mutexattr_settype(&mutexattr, PTHREAD_MUTEX_RECURSIVE_NP);
-		# endif
-		//pthread_mutex_init(&pPthreadLock, &mutexattr);
-		*/
-		//pthread_mutexattr_destroy(&mutexattr);
-		pthread_mutex_init(&pPthreadLock, NULL);
+		if (recursive)
+		{
+			pthread_mutexattr_t mutexattr;
+			pthread_mutexattr_init(&mutexattr);
+			# ifdef YUNI_OS_DARWIN
+			pthread_mutexattr_settype(&mutexattr, PTHREAD_MUTEX_RECURSIVE);
+			# else
+			pthread_mutexattr_settype(&mutexattr, PTHREAD_MUTEX_RECURSIVE_NP);
+			# endif
+			pthread_mutex_init(&pPthreadLock, &mutexattr);
+			pthread_mutexattr_destroy(&mutexattr);
+		}
+		else
+		{
+			pthread_mutex_init(&pPthreadLock, NULL);
+		}
 	}
+
+
 
 } // namespace Yuni
 
