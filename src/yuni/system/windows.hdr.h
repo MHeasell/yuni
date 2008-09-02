0f76a60c1e83c3f3506144a6936a3daa4cc75b8a
milipili <milipili@shikami.org>
2008-09-02 10:00:38 +0000

    System: Windows:  * Fixed includes  * Added our own implementation of gettimeofday for Microsoft Windows (missing on Visual Studio)
    git-svn-id: http://svn.ta3d.org/yuni/trunk@116 7e728022-c0ab-4603-8e65-c666ead28a33

--- src/yuni/system/windows.hdr.h
+++ src/yuni/system/windows.hdr.h
@@ -1,10 +1,15 @@
 #ifndef __YUNI_SYSTEM_WINDOWS_HEADERS_H__
 # define __YUNI_SYSTEM_WINDOWS_HEADERS_H__
 
-# ifndef WIN32_LEAN_AND_MEAN
-#	define WIN32_LEAN_AND_MEAN 1
+
+# include "../yuni.h"
+
+# ifdef YUNI_OS_WINDOWS
+#	ifndef WIN32_LEAN_AND_MEAN
+#		define WIN32_LEAN_AND_MEAN 1
+#	endif
+#	include <windows.h>
 # endif
-# include <windows.h>
 
 
 #endif // __YUNI_SYSTEM_WINDOWS_HEADERS_H__
