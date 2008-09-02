b11c9f2709b4ce454a979801cb0544c003b2006d
milipili <milipili@shikami.org>
2008-09-02 08:48:49 +0000

    Added a header to include the Microsoft C++ Headers (merely windows.h) without not really useful other headers
    git-svn-id: http://svn.ta3d.org/yuni/trunk@115 7e728022-c0ab-4603-8e65-c666ead28a33

---
+++ src/yuni/system/windows.hdr.h
@@ -0,0 +1,10 @@
+#ifndef __YUNI_SYSTEM_WINDOWS_HEADERS_H__
+# define __YUNI_SYSTEM_WINDOWS_HEADERS_H__
+
+# ifndef WIN32_LEAN_AND_MEAN
+#	define WIN32_LEAN_AND_MEAN 1
+# endif
+# include <windows.h>
+
+
+#endif // __YUNI_SYSTEM_WINDOWS_HEADERS_H__
