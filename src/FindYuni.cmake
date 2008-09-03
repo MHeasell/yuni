130fca50ceace05fb6e7de9771bab39072d3c04f
milipili <milipili@shikami.org>
2008-09-03 12:45:35 +0000

    The path to the yuni library is now automatically provided by CMake
    git-svn-id: file:///home/michael/svn/yuni/trunk@127 7e728022-c0ab-4603-8e65-c666ead28a33

--- src/FindYuni.cmake
+++ src/FindYuni.cmake
@@ -4,13 +4,3 @@
 #
 Include_Directories("${PROJECT_SOURCE_DIR}/..")
 
-#
-# The Yuni library
-#
-IF(WIN32 AND MSVC)
-    Set(YUNI_STATIC_LIB "${PROJECT_SOURCE_DIR}/../../bin/static/Debug/yuni.lib")
-Else(WIN32 AND MSVC)
-    Set(YUNI_STATIC_LIB "${PROJECT_SOURCE_DIR}/../../bin/static/libyuni.a")
-EndIF(WIN32 AND MSVC)
-
-
