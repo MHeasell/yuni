80ad9a94fefe0ee8c9756a20292a0686e5bb638e
milipili <milipili@shikami.org>
2008-09-12 14:55:47 +0000

    Added a sample for events
    git-svn-id: file:///home/michael/svn/yuni/trunk@142 7e728022-c0ab-4603-8e65-c666ead28a33

---
+++ src/samples/events/00.simple/main.cpp
@@ -0,0 +1,69 @@
+
+#include <yuni/yuni.h>
+#include <yuni/string.h>
+#include <yuni/misc/event.h>
+#include <iostream>
+
+
+using namespace Yuni;
+
+
+// An event
+//
+// Each template parameter is the type for each argument
+// Here is an event with 2 arguments :
+Event::E2<const Yuni::String&, const Yuni::String&> emailHasBeenReceived;
+
+
+
+
+/*!
+ * A subscriber class
+ *
+ * To be able to receive events, the class must inherit from the base class
+ * `Yuni::Event::Receiver`
+ */
+class Subscriber : public Event::Receiver
+{
+public:
+	//! Constructor
+	Subscriber()
+		:Event::Receiver()
+	{
+		// This class want to be aware when an event is fired
+		emailHasBeenReceived.connect(this, &Subscriber::onMailReceived);
+	}
+
+	//! Destructor
+	virtual ~Subscriber()
+	{
+		// It is really encouraged to disconnect all events as soon as possible.
+		// The base class will do it, however the inherited class will already
+		// be removed from the vtable and it might lead to a critical error
+		disconnectAllNotifiers();
+	}
+
+	/*!
+	 * This method will be called each time the event `emailHasBeenReceived`
+	 * is fired
+	 */
+	void onMailReceived(const String& subject, const String& from)
+	{
+		std::cout << "a mail has been received :" << std::endl
+			<< "\tFrom    : " << from << std::endl
+			<< "\tSubject : " << subject << std::endl;
+	}
+};
+
+
+
+int main(void)
+{
+	// A subscriber, able to receive some events
+	Subscriber sc;
+	// A new event
+	emailHasBeenReceived("My great subject !", "paul@utopia.org");
+
+	return 0;
+}
+
