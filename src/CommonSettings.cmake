63ab558d7fc73143e0d20603ffd7652ee36a9a68
milipili <milipili@shikami.org>
2008-09-12 15:01:00 +0000

    CMake: There are now 2 targets for compiling (Debug / Release)
    git-svn-id: file:///home/michael/svn/yuni/trunk@144 7e728022-c0ab-4603-8e65-c666ead28a33

--- src/CommonSettings.cmake
+++ src/CommonSettings.cmake
@@ -11,6 +11,11 @@ ENDIF(NOT CMAKE_MINOR_VERSION EQUAL 4 OR NOT CMAKE_MAJOR_VERSION EQUAL 2 )
 Include(CheckIncludeFile)
 
 
+
+
+
+
+
 #
 # Informations about the system
 #
@@ -24,29 +29,92 @@ EndIf(MINGW)
 If(XCODE)
     Message(STATUS "Compiler: XCode")
 EndIf(XCODE)
+If(CMAKE_COMPILER_IS_GNUCXX)
+	If(NOT GNUCXX_VERSION)
+		# -dumpversion might be used
+		Execute_process(COMMAND ${CMAKE_CXX_COMPILER} "--version" OUTPUT_VARIABLE GNUCXX_VERSION_N ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE)
+		String(REGEX REPLACE "Copyright.*" "" GNUCXX_VERSION_N "${GNUCXX_VERSION_N}")
+		String(STRIP "${GNUCXX_VERSION_N}" GNUCXX_VERSION_N)
+		SET(GNUCXX_VERSION "${GNUCXX_VERSION_N}" CACHE INTERNAL "Version of the Gnu Gxx compiler")
+	EndIf(NOT GNUCXX_VERSION)
+	Message(STATUS "g++ Variant : ${GNUCXX_VERSION}")
+EndIf(CMAKE_COMPILER_IS_GNUCXX)
+
+
+
+
+
 
 
 #
-# Command line options for G++
+# Command line options for G++ (Debug)
+# 
+# Ex: cmake . -DCMAKE_BUILD_TYPE=release 
 #
-IF(NOT WIN32)
-	String(LENGTH "${CMAKE_CXX_FLAGS}" VA)
-	IF(${VA} EQUAL 0)
-		Set(CMAKE_CXX_FLAGS "-O3 -g -Wall -Werror -Woverloaded-virtual -Wextra -Wshadow -Wconversion -fstack-check -funroll-loops")
-	ENDIF(${VA} EQUAL 0)
-	Message(STATUS "Default C++ flags set to `${CMAKE_CXX_FLAGS}`")
-Else(NOT WIN32)
-	IF(MINGW)
-		Set(CMAKE_CXX_OTHER_FLAGS "-Woverloaded-virtual -Wextra -Wshadow -Wconversion -fstack-check -funroll-loops")
-		Message(STATUS "Added C++ flags `${CMAKE_CXX_OTHER_FLAGS}`")
-		Set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${CMAKE_CXX_OTHER_FLAGS}")
-	ENDIF(MINGW)
-	IF(MSVC)
-		Set(CMAKE_CXX_OTHER_FLAGS "/GR /WX /Ob2 /Ot /MDd")
-		Message(STATUS "Added C++ flags `${CMAKE_CXX_OTHER_FLAGS}`")
-		Set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${CMAKE_CXX_OTHER_FLAGS}")
-	ENDIF(MSVC)
-ENDIF(NOT WIN32)
+IF("${CMAKE_BUILD_TYPE}" STREQUAL "release")
+
+	#
+	# Build Configuration: Release
+	#
+	Message(STATUS "* Build Configuration: Release")
+
+	IF(NOT WIN32)
+		String(LENGTH "${CMAKE_CXX_FLAGS}" VA)
+		IF(${VA} EQUAL 0)
+			Set(CMAKE_CXX_FLAGS "-O3 -fomit-frame-pointer -Wall  -Wextra -mfpmath=sse -msse -msse2")
+			If(APPLE)
+				Set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fvisibility=hidden -isysroot /Developer/SDKs/MacOSX10.4u.sdk -mmacosx-version-min=10.4")
+			ENdif(APPLE)
+		ENDIF(${VA} EQUAL 0)
+		Message(STATUS "Default C++ flags set to `${CMAKE_CXX_FLAGS}`")
+	Else(NOT WIN32)
+		IF(MINGW)
+			Set(CMAKE_CXX_OTHER_FLAGS "-O3 -fomit-frame-pointer -Wextra ")
+			Message(STATUS "Added C++ flags `${CMAKE_CXX_OTHER_FLAGS}`")
+			Set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${CMAKE_CXX_OTHER_FLAGS}")
+		ENDIF(MINGW)
+		IF(MSVC)
+			Set(CMAKE_CXX_OTHER_FLAGS "/Ob2 /Ot /O2 /MDd /Wp64 /Oy /MT /GL /GS- /openmp")
+			Message(STATUS "Added C++ flags `${CMAKE_CXX_OTHER_FLAGS}`")
+			Set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${CMAKE_CXX_OTHER_FLAGS}")
+		ENDIF(MSVC)
+	ENDIF(NOT WIN32)
+	ADD_DEFINITIONS("-DNDEBUG") # Remove asserts
+
+Else("${CMAKE_BUILD_TYPE}" STREQUAL "release")
+	
+	#
+	# Build Configuration: Debug
+	#
+	Message(STATUS "* Build Configuration: Debug")
+
+
+	IF(NOT WIN32)
+		String(LENGTH "${CMAKE_CXX_FLAGS}" VA)
+		IF(${VA} EQUAL 0)
+			Set(CMAKE_CXX_FLAGS "-ggdb2 -gfull -Wall -Woverloaded-virtual -Wextra -Wconversion -fstack-check -funroll-loops")
+			If(APPLE)
+				Set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fvisibility=hidden -isysroot /Developer/SDKs/MacOSX10.4u.sdk -mmacosx-version-min=10.4")
+			ENdif(APPLE)
+		ENDIF(${VA} EQUAL 0)
+		Message(STATUS "Default C++ flags set to `${CMAKE_CXX_FLAGS}`")
+	Else(NOT WIN32)
+		IF(MINGW)
+			Set(CMAKE_CXX_OTHER_FLAGS "-g2 -gfull -Woverloaded-virtual -Wextra -Wconversion -fstack-check -funroll-loops")
+			Message(STATUS "Added C++ flags `${CMAKE_CXX_OTHER_FLAGS}`")
+			Set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${CMAKE_CXX_OTHER_FLAGS}")
+		ENDIF(MINGW)
+		IF(MSVC)
+			Set(CMAKE_CXX_OTHER_FLAGS "/GR /Ob2 /Ot /MDd /MTd /RTCc /fp:except /openmp")
+			Message(STATUS "Added C++ flags `${CMAKE_CXX_OTHER_FLAGS}`")
+			Set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${CMAKE_CXX_OTHER_FLAGS}")
+		ENDIF(MSVC)
+	ENDIF(NOT WIN32)
+
+EndIF("${CMAKE_BUILD_TYPE}" STREQUAL "release")
+
+
+
 
 
 
@@ -64,4 +132,15 @@ Else(CMAKE_USE_WIN32_THREADS_INIT)
 	Link_libraries(${CMAKE_THREAD_LIBS_INIT})
 	Message(STATUS "Threading Support: PThreads (${CMAKE_THREAD_LIBS_INIT})")
 EndIF(CMAKE_USE_WIN32_THREADS_INIT)
+
+
+
+
+
+#
+# 3D Back-end
+#
+ADD_DEFINITIONS("-DYUNI_EXTERNAL_3D_IRRLICHT")
+INCLUDE_DIRECTORIES("${PROJECT_SOURCE_DIR}/../../externals/irrlicht/include")
+Set(YUNI_EXTERNAL_3D_IRRLICHT "1")
 
