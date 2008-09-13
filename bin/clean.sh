4f3b962a65ea086ba38e5b6044f5b790382f17dd
milipili <milipili@shikami.org>
2008-09-13 14:25:54 +0000

    Added XCode projects in the cleaning process
    git-svn-id: file:///home/michael/svn/yuni/trunk@153 7e728022-c0ab-4603-8e65-c666ead28a33

--- bin/clean.sh
+++ bin/clean.sh
@@ -6,5 +6,6 @@ find "${root}/../src" '(' -name 'cmake*' \
             -or -name 'CMakeCache*' \
             -or -name 'CMakeFiles' \
             -or -name 'Makefile' \
+			-or -name '*.xcodeproj' \
             -or -name '*.o' \
             -or -name '.*.swp' ')' -exec echo ' * Removing `' '{}' '`' \; -exec rm -rf '{}' \;
