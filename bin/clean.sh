563e820849675f133fa39c8f06e9f9953ec7846a
milipili <milipili@shikami.org>
2008-09-13 14:29:35 +0000

    CMakeScripts used by XCode projects (which are generated by CMake) should be removed too in the cleaning process
    git-svn-id: http://svn.ta3d.org/yuni/trunk@154 7e728022-c0ab-4603-8e65-c666ead28a33

--- bin/clean.sh
+++ bin/clean.sh
@@ -7,5 +7,6 @@ find "${root}/../src" '(' -name 'cmake*' \
             -or -name 'CMakeFiles' \
             -or -name 'Makefile' \
 			-or -name '*.xcodeproj' \
+			-or -name 'CMakeScripts' \
             -or -name '*.o' \
             -or -name '.*.swp' ')' -exec echo ' * Removing `' '{}' '`' \; -exec rm -rf '{}' \;
