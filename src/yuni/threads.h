b188a16cc1ca9075295b1b702c13948e64f32c72
milipili <milipili@shikami.org>
2008-09-13 22:20:15 +0000

    Fixed wrong include paths (they are no longer in the system paths)
    git-svn-id: file:///home/michael/svn/yuni/trunk@157 7e728022-c0ab-4603-8e65-c666ead28a33

--- src/yuni/threads.h
+++ src/yuni/threads.h
@@ -1,9 +1,11 @@
 #ifndef __YUNI_THREADS_H__
 # define __YUNI_THREADS_H__
 
-# include <yuni/yuni.h>
-# include <yuni/threads/mutex.h>
-# include <yuni/threads/thread.h>
-# include <yuni/threads/timer.h>
+
+# include "yuni.h"
+# include "threads/mutex.h"
+# include "threads/thread.h"
+# include "threads/timer.h"
+
 
 #endif // __YUNI_THREADS_H__
