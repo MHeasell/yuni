8a91030b46c0d291dfd6cd126e28a064898dbc6b
loomchild <loomchild@7e728022-c0ab-4603-8e65-c666ead28a33>
2008-09-14 16:49:13 +0000

    Added metaballs / implicit surfaces code.
    git-svn-id: http://svn.ta3d.org/yuni/trunk@160 7e728022-c0ab-4603-8e65-c666ead28a33

---
+++ src/yuni/gfx/implicitsurface.cpp
@@ -0,0 +1,50 @@
+
+#include "implicitsurface.h"
+
+namespace W
+{
+namespace GUI3D
+{
+
+    ImplicitSurface::ImplicitSurface()
+    {
+    }
+    
+    ImplicitSurface::~ImplicitSurface()
+    {
+    }
+
+    /*! Create a new metaball
+    ** It becomes the responsibility of the ImplicitSurface, so
+    ** it must not be deleted elsewhere.
+    */
+    MetaBall* ImplicitSurface::addMetaBall(const Point& p, float density)
+    {
+        MetaBall* newMB = new MetaBall(p, density);
+        pObjects.push_back(newMB);
+        return newMB;
+    }
+
+    MetaBall* ImplicitSurface::addMetaBall(float x, float y, float z, float density)
+    {
+        MetaBall* newMB = new MetaBall(x, y, z, density);
+        pObjects.push_back(newMB);
+        return newMB;
+    }
+
+    /*! Calculate the density of a point regarding the isosurface
+    ** The density is the sum of all the densities of the metaobjects
+    ** defining the isosurface.
+    */
+    float ImplicitSurface::operator()(const Point& p) const
+    {
+        float res = 0.0f;
+        for (std::vector<MetaObject*>::const_iterator it = pObjects.begin();
+             it != pObjects.end();
+             ++it)
+            res += (**it)(p);
+        return res;
+    }
+
+} // GUI3D
+} // W
