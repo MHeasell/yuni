3572b5a4e31bad8f6845e852ab36efaf4d846be9
milipili <milipili@shikami.org>
2008-09-18 14:41:23 +0000

    CMake: Smarter linking for external libraries (such as Irrlicht)
    git-svn-id: http://svn.ta3d.org/yuni/trunk@180 7e728022-c0ab-4603-8e65-c666ead28a33

--- src/FindYuni.cmake
+++ src/FindYuni.cmake
@@ -1,7 +1,27 @@
+#
+# YUNI_INCLUDE
+#
+
 
 #
 # The common paths for Yuni headers
 #
-Include_Directories("${PROJECT_SOURCE_DIR}/..")
+SET(YUNI_INCLUDE "${PROJECT_SOURCE_DIR}/..")
 
-Link_libraries("${PROJECT_SOURCE_DIR}/../../externals/irrlicht/lib/libIrrlicht.a")
+SET(YUNI_LIBRARIES  yuni-static)
+
+IF(APPLE)
+	SET(YUNI_LIBRARIES  ${YUNI_LIBRARIES} "-framework Cocoa")
+ENDIF(APPLE)
+include_directories(${YUNI_INCLUDE})
+
+#
+# Irrlicht
+#
+SET(YUNI_EXT_IRRLICHT_INCLUDE "${PROJECT_SOURCE_DIR}/../../externals/irrlicht/include")
+SET(YUNI_EXT_IRRLICHT_LIB
+		"${PROJECT_SOURCE_DIR}/../../externals/irrlicht/lib/libIrrlicht.a"
+		"-framework Foundation -framework AppKit -framework Cocoa -framework Carbon -framework OpenGL")
+
+
+# Link_libraries("${PROJECT_SOURCE_DIR}/../../externals/irrlicht/lib/libIrrlicht.a")
