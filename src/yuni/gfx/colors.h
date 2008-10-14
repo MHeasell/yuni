20387bb58cf166974b570fae91dfe7aefdd91768
milipili <milipili@shikami.org>
2008-10-14 08:46:45 +0000

    Added a missing file for the documentation
    git-svn-id: file:///home/michael/svn/yuni/trunk@234 7e728022-c0ab-4603-8e65-c666ead28a33

---
+++ src/yuni/gfx/colors.h
@@ -0,0 +1,27 @@
+#ifndef __YUNI_GFX_COLORS_H__
+# define __YUNI_GFX_COLORS_H__
+
+
+namespace Yuni
+{
+namespace Gfx
+{
+
+/*!
+** \brief Color models
+*/
+namespace Color
+{
+} // namespace Color
+} // namespace Gfx
+} // namespace Yuni
+
+/*!
+** \defgroup ColorModels Color models
+*/
+
+
+# include "rgba.h"
+# include "rgb.h"
+
+#endif // __YUNI_GFX_COLORS_H__
