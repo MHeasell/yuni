a9ce2d5619d3d775b831892ba633d07b9bb3d5eb
milipili <milipili@shikami.org>
2008-10-14 10:10:43 +0000

    Added the conversion & checking policies
    git-svn-id: http://svn.ta3d.org/yuni/trunk@235 7e728022-c0ab-4603-8e65-c666ead28a33

---
+++ src/yuni/policies/conversions.h
@@ -0,0 +1,51 @@
+#ifndef __YUNI_POLICIES_CONVERSIONS_H__
+# define __YUNI_POLICIES_CONVERSIONS_H__
+
+# include "policies.h"
+
+
+namespace Yuni
+{
+namespace Policy
+{
+namespace Conversion
+{
+
+	/*!
+	** \defgroup ConversionPolicies Conversion policies
+	** \ingroup Policies
+	*/
+
+
+	/*! \class Allow
+	** \ingroup ConversionPolicies
+	*/
+	struct Allow
+	{
+		enum { allow = true };
+	};
+
+
+	/*! \class Disallow
+	** \ingroup ConversionPolicies
+	*/
+	struct Disallow
+	{
+		//! Default constructor
+		Disallow() {}
+		/*!
+		** \brief Copy constructor
+		**
+		** It is possible to initialize a `Disallow` policy with an `Allow` policy 
+		*/
+		Disallow(const Allow&) {}
+
+		enum { allow = false };
+	};
+
+
+} // namespace Conversion
+} // namespace Policy
+} // namespace Yuni
+
+#endif // __YUNI_POLICIES_CONVERSIONS_H__
