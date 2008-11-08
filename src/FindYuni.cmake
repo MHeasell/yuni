0469a690f21befb93b7699ea91e6da420271530a
milipili <milipili@shikami.org>
2008-11-08 23:56:18 +0000

    Welcome to the DevPacks !
    git-svn-id: file:///home/michael/svn/yuni/trunk@247 7e728022-c0ab-4603-8e65-c666ead28a33

--- src/FindYuni.cmake
+++ src/FindYuni.cmake
@@ -13,19 +13,13 @@ SET(YUNI_LIBRARIES  yuni-static)
 IF(APPLE)
 	SET(YUNI_LIBRARIES  ${YUNI_LIBRARIES} "-framework Cocoa")
 ENDIF(APPLE)
-include_directories(${YUNI_INCLUDE})
+Include_Directories(${YUNI_INCLUDE})
+
 
 #
 # Irrlicht
 #
-SET(YUNI_EXT_IRRLICHT_INCLUDE "${PROJECT_SOURCE_DIR}/../../externals/irrlicht/include")
-SET(YUNI_EXT_IRRLICHT_LIB
-		"${PROJECT_SOURCE_DIR}/../../externals/irrlicht/lib/libIrrlicht.a")
-IF(APPLE)
-		Set(YUNI_EXT_IRRLICHT_LIB ${YUNI_EXT_IRRLICHT_LIB} "-framework Foundation -framework AppKit -framework Cocoa -framework Carbon -framework OpenGL")
-Else(APPLE)
-	IF(UNIX)
-		Set(YUNI_EXT_IRRLICHT_LIB ${YUNI_EXT_IRRLICHT_LIB} " -L/usr/X11R6/lib -lGL -lXxf86vm  -lX11")
-	EndIF(UNIX)
-EndIF(APPLE)
+If(YUNI_EXTERNAL_3D_IRRLICHT)
+	DEVPACK_IMPORT_IRRLICHT()
+Endif(YUNI_EXTERNAL_3D_IRRLICHT)
 
