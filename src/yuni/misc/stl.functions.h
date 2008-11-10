12328a6fe78da000fe6b0271a1be63a72934e83c
milipili <milipili@shikami.org>
2008-11-10 22:06:21 +0000

    Added a missing include and a missing namespace
    git-svn-id: file:///home/michael/svn/yuni/trunk@253 7e728022-c0ab-4603-8e65-c666ead28a33

--- src/yuni/misc/stl.functions.h
+++ src/yuni/misc/stl.functions.h
@@ -1,18 +1,21 @@
-
 #ifndef __YUNI_MISC_STL_FUNCTIONS_H__
 # define __YUNI_MISC_STL_FUNCTIONS_H__
+
+# include <algorithm>
+
+
 
 namespace Yuni
 {
 namespace Misc
 {
+
 	/*!
 	** \brief Functor to delete a pointer: used in automatic clearing of an STL container
 	*/
 	template <typename T>
-	class STLDelete
+	struct STLDelete
 	{
-	public:
 		STLDelete& operator () (T* ptr)
 		{
 			delete ptr;
@@ -20,15 +23,19 @@ namespace Misc
 		}
 	};
 
+
+
 	/*!
 	** \brief Syntactic sugar to free all pointers in an STL container and clear it
 	*/
 	template <typename T, typename U>
 	void STLDeleteAndClear(U& container)
 	{
-		for_each(container.begin(), container.end(), STLDelete<T>());
+		std::for_each(container.begin(), container.end(), STLDelete<T>());
 		container.clear();
 	}
+
+
 
 } // Misc
 } // Yuni
