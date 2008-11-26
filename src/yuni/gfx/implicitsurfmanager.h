9e7b6c61e41ccee8f58d01064835091cf345a767
milipili <milipili@shikami.org>
2008-11-26 22:26:38 +0000

    Typo
    git-svn-id: file:///home/michael/svn/yuni/trunk@261 7e728022-c0ab-4603-8e65-c666ead28a33

--- src/yuni/gfx/implicitsurfmanager.h
+++ src/yuni/gfx/implicitsurfmanager.h
@@ -18,18 +18,26 @@ namespace Gfx
 	class ImplicitSurfManager
 	{
 	public:
+		//! \name Constructor & Destructor
+		//@{
+		//! Default constructor
 		ImplicitSurfManager();
+		//! Destructor
 		~ImplicitSurfManager();
+		//@}
 
 		/*!
 		** \brief Create a new metaball
+		**
 		** \param p Center of the metaball
 		** \param density Density of the metaball (~= radius)
 		** \result The newly created MetaBall object
 		*/
 		MetaBall* addMetaBall(const Point& p, float density);
+
 		/*!
 		** \brief Create a new metaball
+		**
 		** \param x X coordinate for the center of the metaball
 		** \param y Y coordinate for the center of the metaball
 		** \param z Z coordinate for the center of the metaball
@@ -38,9 +46,13 @@ namespace Gfx
 		*/
 		MetaBall* addMetaBall(float x, float y, float z, float density);
 
+		/*!
+		** \todo
+		*/
 		float calculateSurface();
 
 	private:
+		//! List of meta-objects in the manager
 		std::vector<SharedPtr<MetaObject> > pObjects;
 
 	}; // class ImplicitSurfManager
