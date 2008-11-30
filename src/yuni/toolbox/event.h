50ee3650f0953e87aca16760ffedeca355108b67
milipili <milipili@shikami.org>
2008-11-30 23:50:25 +0000

    Added missing headers
    git-svn-id: http://svn.ta3d.org/yuni/trunk@274 7e728022-c0ab-4603-8e65-c666ead28a33

---
+++ src/yuni/toolbox/event.h
@@ -0,0 +1,6 @@
+#ifndef __YUNI_TOOLBOX_EVENT_H__
+# define __YUNI_TOOLBOX_EVENT_H__
+
+# include "event/event.h"
+
+#endif // __YUNI_TOOLBOX_EVENT_H__
---
+++ src/yuni/toolbox/string.h
@@ -0,0 +1,7 @@
+#ifndef __YUNI_TOOLBOX_STRING_H__
+# define __YUNI_TOOLBOX_STRING_H__
+
+// The real declaration is in `toolbox/string/string.h`
+# include "string/string.h"
+
+#endif // __YUNI_TOOLBOX_STRING_H__
