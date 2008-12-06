223d12d58318ab96bb5c585cb945b053762d6c8b
milipili <milipili@shikami.org>
2008-12-06 19:28:16 +0000

    Moved the routines for the handling of files & folders
    git-svn-id: file:///home/michael/svn/yuni/trunk@283 7e728022-c0ab-4603-8e65-c666ead28a33

--- src/yuni/files.h
+++
@@ -1,6 +0,0 @@
-#ifndef __YUNI_FILES_H__
-# define __YUNI_FILES_H__
-
-# include "misc/files.h"
-
-#endif // __YUNI_FILES_H__
--- src/yuni/paths.h
+++
@@ -1,8 +0,0 @@
-#ifndef __YUNI_PATHS_H__
-# define __YUNI_PATHS_H__
-
-
-# include "misc/paths.h"
-
-
-#endif // __YUNI_PATHS_H__
