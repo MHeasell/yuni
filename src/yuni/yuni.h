d4677dd84bbf88ba2dfce96785b3322f60e46e81
milipili <milipili@shikami.org>
2008-12-06 19:41:57 +0000

    Added some documentation
    git-svn-id: file:///home/michael/svn/yuni/trunk@285 7e728022-c0ab-4603-8e65-c666ead28a33

--- src/yuni/yuni.h
+++ src/yuni/yuni.h
@@ -1,6 +1,27 @@
 #ifndef __YUNI_COMMONS_H__
 # define __YUNI_COMMONS_H__
 
+/*
+** The Yuni project is a high-level cross-platform framework. This framework
+** intend to provide the most complete set as possible of coherent API related
+** to 3D programming, especially game programming. It intends to be a reliable and
+** simple bridge between different worlds to make all sort of applications,
+** letting the user focus on its real work.
+**
+** The Yuni project is connected with all those domains :
+** - 3D graphics
+** - Physics
+** - 2D/3D User Interface in 3D context
+** - 2D/3D Input devices
+** - Sound, playback of 2D/3D sounds
+** - Scripting languages
+** - Artificial Intelligence, mainly on 3D objects
+** - Threading / Parallel computing
+** - Networking
+** - Real-time and multi-user Collaboration
+** - Any other domain related to multimedia or user interaction.
+*/
+
 
 //! \name Informations about the Yuni Library
 //@{
