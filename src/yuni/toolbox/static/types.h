b696fa69bc978b1edbe75011bcd24d6b640eac65
milipili <milipili@shikami.org>
2008-12-06 23:28:18 +0000

    Added tools to determine types at compile-time
    git-svn-id: http://svn.ta3d.org/yuni/trunk@289 7e728022-c0ab-4603-8e65-c666ead28a33

---
+++ src/yuni/toolbox/static/types.h
@@ -0,0 +1,114 @@
+#ifndef __YUNI_TOOLBOX_STATIC_TYPES_H__
+# define __YUNI_TOOLBOX_STATIC_TYPES_H__
+
+# include "../../yuni.h"
+
+
+namespace Yuni
+{
+namespace Static
+{
+namespace Type
+{
+
+	//! \name Fundamental types
+	//@{
+
+	/*!
+	** \brief Get if a type is fundamental
+	*/
+	template <typename T>
+	struct IsFundamental  { enum { Yes = 0, No = 1} };
+
+
+	// Those types are fundamental for sure
+	//
+	template <> struct IsFundamental<void> {enum{Yes = 1, No = 0}};
+	template <> struct IsFundamental<bool> {enum{Yes = 1, No = 0}};
+	template <> struct IsFundamental<char> {enum{Yes = 1, No = 0}};
+	template <> struct IsFundamental<unsigned char> {enum{Yes = 1, No = 0}};
+	template <> struct IsFundamental<signed char> {enum{Yes = 1, No = 0}};
+	template <> struct IsFundamental<wchar_t> {enum{Yes = 1, No = 0}};
+	template <> struct IsFundamental<float> {enum{Yes = 1, No = 0}};
+	template <> struct IsFundamental<double> {enum{Yes = 1, No = 0}};
+	template <> struct IsFundamental<long double> {enum{Yes = 1, No = 0}};
+	// Signed int
+	template <> struct IsFundamental<sint8> {enum{Yes = 1, No = 0}};
+	template <> struct IsFundamental<sint16> {enum{Yes = 1, No = 0}};
+	template <> struct IsFundamental<sint32> {enum{Yes = 1, No = 0}};
+	template <> struct IsFundamental<sint64> {enum{Yes = 1, No = 0}};
+	// Unsigned int
+	template <> struct IsFundamental<uint8> {enum{Yes = 1, No = 0}};
+	template <> struct IsFundamental<uint16> {enum{Yes = 1, No = 0}};
+	template <> struct IsFundamental<uint32> {enum{Yes = 1, No = 0}};
+	template <> struct IsFundamental<uint64> {enum{Yes = 1, No = 0}};
+
+	//@}
+
+
+	//! \name Cmpound types
+	//@{
+
+
+	/*!
+	** \brief Determine the compound type
+	*/
+	template <typename T>
+	struct Compound
+	{
+		enum { IsPointer = 0, IsReference = 0, IsArray = 0, IsPtrMember = 0 };
+		typedef T BaseType;
+	};
+
+	// Specialization for references
+	template <typename T>
+	struct Compound<T&>
+	{
+		enum { IsPointer = 0, IsReference = 1, IsArray = 0, IsPtrMember = 0 };
+		typedef typename Compound<T>::BaseType BaseType;
+	};
+
+	// Specialization for pointers
+	template <typename T>
+	struct Compound<T*>
+	{
+		enum { IsPointer = 1, IsReference = 0, IsArray = 0, IsPtrMember = 0 };
+		typedef typename Compound<T>::BaseType BaseType;
+	};
+
+	// Specialization for empty arrays
+	template <typename T>
+	struct Compound<T[]>
+	{
+		enum { IsPointer = 0, IsReference = 0, IsArray = 1, IsPtrMember = 0 };
+		typedef typename Compound<T>::BaseType BaseType;
+	};
+
+	// Specialization for arrays
+	template <typename T, size_t N>
+	struct Compound<T[N]>
+	{
+		enum { IsPointer = 0, IsReference = 0, IsArray = 1, IsPtrMember = 0 };
+		typedef typename Compound<T>::BaseType BaseType;
+	};
+
+	// Specialization for pointer-to members
+	template <typename T, typename C>
+	struct Compound<T C::*>
+	{
+		enum { IsPointer = 0, IsReference = 0, IsArray = 0, IsPtrMember = 1 };
+		typedef typename Compound<T>::BaseType BaseType;
+	};
+
+
+
+
+	//@} // Compound types
+
+
+} // namespace Type
+} // namespace Static
+} // namespace Yuni
+
+
+#endif // __YUNI_TOOLBOX_STATIC_TYPES_H__
