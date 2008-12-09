444ba0453776910c9d9b1e0e05ecf3097e3af3c3
milipili <milipili@shikami.org>
2008-12-09 21:01:24 +0000

    Added missing header files
    git-svn-id: http://svn.ta3d.org/yuni/trunk@290 7e728022-c0ab-4603-8e65-c666ead28a33

---
+++ src/yuni/toolbox/files.h
@@ -0,0 +1,6 @@
+#ifndef __YUNI_TOOLBOX_FILES_H__
+# define __YUNI_TOOLBOX_FILES_H__
+
+# include "fs/files.h"
+
+#endif // __YUNI_TOOLBOX_FILES_H__
---
+++ src/yuni/toolbox/paths.h
@@ -0,0 +1,6 @@
+#ifndef __YUNI_TOOLBOX_PATHS_H__
+# define __YUNI_TOOLBOX_PATHS_H__
+
+# include "fs/paths.h"
+
+#endif // __YUNI_TOOLBOX_PATHS_H__
