47528f4786d7439999f28814d0a78e275c35aa3c
milipili <milipili@shikami.org>
2008-12-29 21:33:35 +0000

    The devpack for irrlicht is not required when the gfx3d module is disabled
    git-svn-id: file:///home/michael/svn/yuni/trunk@314 7e728022-c0ab-4603-8e65-c666ead28a33

--- src/Modules.cmake
+++ src/Modules.cmake
@@ -6,7 +6,7 @@
 # Core
 Set(YUNI_MODULE_CORE                      TRUE) # Must be equals to True
 # Gfx3D
-Set(YUNI_MODULE_GFX3D                     TRUE)
+Set(YUNI_MODULE_GFX3D                     FALSE)
 	Set(YUNI_EXTERNAL_3D_IRRLICHT         TRUE)
 # Scripts
 Set(YUNI_MODULE_SCRIPTS                   TRUE)
