6073baa7746defb1d486770e68ccacb6e66699fa
footy <footy@7e728022-c0ab-4603-8e65-c666ead28a33>
2008-12-29 21:38:07 +0000

    * Added a DevPack for Lua 5.1.4  * Added a skeleton for the Script module.  * Created a basic skeleton for the future Script module sample (it's just a placeholder).  * The DevPack is available only for OS X, so the Script module is disabled by default for the moment.
    git-svn-id: http://svn.ta3d.org/yuni/trunk@315 7e728022-c0ab-4603-8e65-c666ead28a33

---
+++ src/samples/script/00.helloworld/main.cpp
@@ -0,0 +1,10 @@
+/*!
+ * This is a placeholder for the helloworld sample for the script
+ * module. It is *not functional* yet, so it returns an error.
+ */
+
+int main()
+{
+	// Return an error.
+	return 1;
+}
