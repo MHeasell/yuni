7c40cf7fa0d603c1af559286d78e6f1364b4c1f5
milipili <milipili@shikami.org>
2009-01-04 09:50:03 +0000

    Minor update about the auto-detection of the capabilities of the system :  * Added comments  * Added the `ALWAYS_INLINE` define
    git-svn-id: http://svn.ta3d.org/yuni/trunk@322 7e728022-c0ab-4603-8e65-c666ead28a33

--- src/yuni/toolbox/system/capabilities.h
+++ src/yuni/toolbox/system/capabilities.h
@@ -2,13 +2,10 @@
 # define __YUNI_SYSTEM_OS_DETECTION_H__
 
 /*!
-** \brief Operating System / Architecture Detection
-*/
-
-
-
-/*
-** Usefull defines
+** \brief Operating System / Capabilities Auto-Detection
+**
+**
+** Usefull defines :
 **
 ** - Microsoft Windows
 ** YUNI_OS_WINDOWS: The Microsoft Windows Operating system (32/64Bits)
@@ -73,77 +70,103 @@
 
 
 # if defined(__TOS_WIN__) || defined(__WIN32__) || defined(_WIN64) || defined(_WIN32)
-// Windows
+
+/* Microsoft Windows */
 #	define YUNI_OS_WINDOWS
+/* Windows 64 */
 #	ifdef _WIN64
 #	   define YUNI_OS_WIN64
 #	   define YUNI_OS_64
 #	else
 #	   define YUNI_OS_WIN32
 #	endif
+/* Windows CE */
 #	ifdef _WIN32_WCE
 #	   define YUNI_OS_WINCE
 #	endif
-#	ifdef _MSC_VER // Visual Studio
+/* Visual Studio */
+#	ifdef _MSC_VER
 #	   define YUNI_OS_MSVC
 #	endif
+/* Borland Compiler */
 #	ifdef __BORLANDC__
 #	   define YUNI_OS_BORLAND
 #	endif
+/* Cygwin */
 #	ifdef __CYGWIN__
 #	   define YUNI_OS_CYGWIN
 #	endif
+/* MinGW */
 #	ifdef __MINGW32__
 #	   define YUNI_OS_MINGW
 #	endif
-# else
-// Unixes
+
+
+# else /* if Windows */
+
+/* BeOS */
 # ifdef __BEOS__
 #	define YUNI_OS_BEOS
 # else
+/* Unix compliant */
 #	define YUNI_OS_UNIX
+/* AIX */
 #	ifdef _AIX
 #		define YUNI_OS_AIX
 #	endif
+/* DragonFly */
 #	ifdef __DragonFly__
 #		define YUNI_OS_DRAGONFLY
 #	endif
+/* Linux */
 #	if defined(__linux) || defined(linux)
 #		define YUNI_OS_LINUX
 #	else
+/* FreeBSD */
 #		ifdef __FreeBSD__
 #			define YUNI_OS_FREEBSD __FreeBSD__
 #		endif
+/* HP-UX */
 #		ifdef _hpux
 #			define YUNI_OS_HPUX
 #		endif
+/* Mac OS */
 #		if defined(__APPLE__) || defined(__MACH__)
 #			define YUNI_OS_MAC
 #			define YUNI_OS_DARWIN
 #		endif
+/* NetBSD */
 #		ifdef __NetBSD__
 #			define YUNI_OS_NETBSD
 #		endif
+/* OpenBSD */
 #		ifdef __OpenBSD__
 #			define YUNI_OS_OPENBSD
 #		endif
 #		if defined(sun) || defined(__sun)
+/* Solaris (<= 4.x) */
 #			if defined(__SVR4) || defined(__svr4__)
 #				 define YUNI_OS_SOLARIS
 #			else
+/* SunOS (>= 5.x) */
 #				 define YUNI_OS_SUNOS
 #			endif
 #			if defined(__SUNPRO_C) || defined(__SUNPRO_CC)
 #				 define YUNI_OS_SUNSTUDIO
 #			endif
-#		endif // Sun/Solaris
-#	endif // Linux
+#		endif /* Sun/Solaris */
+#	endif /* Linux */
+/* Lynx */
 #	ifdef __Lynx__
 #		define YUNI_OS_LYNX
 #	endif
-# endif // BEOS
-# endif // Microsoft Windows
+# endif /* BEOS */
+# endif /* Microsoft Windows */
  
+
+
+
+/* GNU project C and C++ compiler */
 # ifdef __GNUC__
 #	define YUNI_OS_GCC
 #	define YUNI_OS_GCC_VERSION (__GNUC__ * 10000 + __GNUC_MINOR__ * 100 + __GNUC_PATCHLEVEL__)
@@ -151,10 +174,14 @@
 #	define YUNI_OS_GCC_VERSION 0 
 # endif
 
+/* Intel Compiler */
 # ifdef __INTEL_COMPILER
 #	define YUNI_OS_INTELCOMPILER
 # endif
 
+
+
+/* 32/64 Bits modes */
 # if !defined(YUNI_OS_32) && !defined(YUNI_OS_64)
 #	if defined(__IA64__) || defined(_IA64) || defined(__amd64__) || defined(__x86_64__)
 #		 define YUNI_OS_64
@@ -165,6 +192,7 @@
 
 
 
+/* export */
 # if defined(_MSC_VER) || defined(__CYGWIN__) || defined(__MINGW32__) || defined(__BCPLUSPLUS__)  || defined(__MWERKS__)
 #	if defined(YUNI_LIBRARY_STATIC)
 #		define YUNI_EXPORT
@@ -178,4 +206,15 @@
 # endif 
 
 
-#endif // __YUNI_SYSTEM_OS_DETECTION_H__
+
+
+/* inline */
+# ifdef YUNI_OS_GCC
+#	define ALWAYS_INLINE  __attribute__((always_inline))
+# else
+#	define ALWAYS_INLINE 
+# endif
+
+
+
+#endif /* __YUNI_SYSTEM_OS_DETECTION_H__ */
