a9b811ecaddae03c1ed0bb46635be7a01e653209
milipili <milipili@shikami.org>
2009-01-09 22:10:25 +0000

    Fixed a lot of bugs...
    git-svn-id: file:///home/michael/svn/yuni/trunk@333 7e728022-c0ab-4603-8e65-c666ead28a33

--- src/yuni/toolbox/static/types.h
+++ src/yuni/toolbox/static/types.h
@@ -2,6 +2,7 @@
 # define __YUNI_TOOLBOX_STATIC_TYPES_H__
 
 # include "../../yuni.h"
+# include "remove.h"
 
 
 namespace Yuni
@@ -32,20 +33,63 @@ namespace Type
 	template <typename T>
 	struct Compound
 	{
-		enum { IsPointer = 0, IsReference = 0, IsArray = 0, IsPtrMember = 0 };
+		//! The original type
 		typedef T BaseType;
+		enum { IsPointer = 0, IsReference = 0, IsArray = 0, IsPtrMember = 0 };
+	};
+	//@}
+
+
+	//! \name Constness
+	//@{
+	template<typename T>
+	struct IsConst
+	{
+		enum { Yes = 0, No = 1 };
 	};
 	//@}
 
 
 	//! \name Comparison
 	//@{
+
+	/*!
+	** \brief Determine if two types are stricly identical
+	**
+	** \code
+	**	std::cout << StrictlyEqual<int, int>::Yes << std::endl; // print 1
+	**	std::cout << StrictlyEqual<int, int&>::Yes << std::endl; // print 1
+	**	std::cout << StrictlyEqual<int, bool>::Yes << std::endl; // print 0
+	** \endcode
+	*/
 	template<typename T, typename U>
-	struct AreEqual
+	struct StrictlyEqual
 	{
 		// Two types are not equal by default
 		enum { Yes = 0, No = 1 };
 	};
+
+	/*!
+	** \brief Determine if two types (without any identifier) are identical
+	**
+	** \code
+	**	std::cout << Equal<int, int>::Yes << std::endl; // print 1
+	**	std::cout << Equal<int, int&>::Yes << std::endl; // print 0
+	**	std::cout << Equal<int, bool>::Yes << std::endl; // print 0
+	** \endcode
+	*/
+	template<typename A, typename B>
+	struct Equal
+	{
+		typedef typename Static::Remove::All<A>::Type  BaseTypeA;
+		typedef typename Static::Remove::All<B>::Type  BaseTypeB;
+		enum
+		{
+			Yes = StrictlyEqual<BaseTypeA, BaseTypeB>::Yes,
+			No  = StrictlyEqual<BaseTypeA, BaseTypeB>::No
+		};
+	};
+
 	//@}
 
 
@@ -60,25 +104,69 @@ namespace Type
 
 	// Those types are fundamental for sure
 	//
-	template <> struct IsFundamental<void> {enum{Yes = 1, No = 0}};
-	template <> struct IsFundamental<bool> {enum{Yes = 1, No = 0}};
-	template <> struct IsFundamental<char> {enum{Yes = 1, No = 0}};
-	template <> struct IsFundamental<unsigned char> {enum{Yes = 1, No = 0}};
-	template <> struct IsFundamental<signed char> {enum{Yes = 1, No = 0}};
-	template <> struct IsFundamental<wchar_t> {enum{Yes = 1, No = 0}};
-	template <> struct IsFundamental<float> {enum{Yes = 1, No = 0}};
-	template <> struct IsFundamental<double> {enum{Yes = 1, No = 0}};
-	template <> struct IsFundamental<long double> {enum{Yes = 1, No = 0}};
+	template <> struct IsFundamental<bool> {enum{Yes = 1, No = 0};};
+	template <> struct IsFundamental<signed char> {enum{Yes = 1, No = 0};};
+	template <> struct IsFundamental<wchar_t> {enum{Yes = 1, No = 0};};
+	template <> struct IsFundamental<float> {enum{Yes = 1, No = 0};};
+	template <> struct IsFundamental<double> {enum{Yes = 1, No = 0};};
+	template <> struct IsFundamental<long double> {enum{Yes = 1, No = 0};};
 	// Signed int
-	template <> struct IsFundamental<sint8> {enum{Yes = 1, No = 0}};
-	template <> struct IsFundamental<sint16> {enum{Yes = 1, No = 0}};
-	template <> struct IsFundamental<sint32> {enum{Yes = 1, No = 0}};
-	template <> struct IsFundamental<sint64> {enum{Yes = 1, No = 0}};
+	template <> struct IsFundamental<sint8>  {enum{Yes = 1, No = 0};};
+	template <> struct IsFundamental<sint16> {enum{Yes = 1, No = 0};};
+	template <> struct IsFundamental<sint32> {enum{Yes = 1, No = 0};};
+	template <> struct IsFundamental<sint64> {enum{Yes = 1, No = 0};};
 	// Unsigned int
-	template <> struct IsFundamental<uint8> {enum{Yes = 1, No = 0}};
-	template <> struct IsFundamental<uint16> {enum{Yes = 1, No = 0}};
-	template <> struct IsFundamental<uint32> {enum{Yes = 1, No = 0}};
-	template <> struct IsFundamental<uint64> {enum{Yes = 1, No = 0}};
+	template <> struct IsFundamental<uint8>  {enum{Yes = 1, No = 0};};
+	template <> struct IsFundamental<uint16> {enum{Yes = 1, No = 0};};
+	template <> struct IsFundamental<uint32> {enum{Yes = 1, No = 0};};
+	template <> struct IsFundamental<uint64> {enum{Yes = 1, No = 0};};
+
+	template <typename T> struct IsFundamental<const T>
+	{
+		enum
+		{
+			Yes = IsFundamental<T>::Yes,
+			No  = IsFundamental<T>::No
+		};
+	};
+
+	template <typename T> struct IsFundamental<T*>
+	{
+		enum
+		{
+			Yes = IsFundamental<T>::Yes,
+			No  = IsFundamental<T>::No
+		};
+	};
+
+
+	template <typename T> struct IsFundamental<T&>
+	{
+		enum
+		{
+			Yes = IsFundamental<T>::Yes,
+			No  = IsFundamental<T>::No
+		};
+	};
+
+	template <typename T> struct IsFundamental<const T*>
+	{
+		enum
+		{
+			Yes = IsFundamental<T>::Yes,
+			No  = IsFundamental<T>::No
+		};
+	};
+
+
+	template <typename T> struct IsFundamental<const T&>
+	{
+		enum
+		{
+			Yes = IsFundamental<T>::Yes,
+			No  = IsFundamental<T>::No
+		};
+	};
 
 
 
@@ -125,7 +213,14 @@ namespace Type
 
 	
 	template<typename W>
-	struct AreEqual<W, W>
+	struct StrictlyEqual<W, W>
+	{
+		enum { Yes = 1, No = 0 };
+	};
+
+	
+	template<typename T>
+	struct IsConst<const T>
 	{
 		enum { Yes = 1, No = 0 };
 	};
