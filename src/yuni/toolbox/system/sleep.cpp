be2756c8106a6ed4a49bb1476a44f8065a51fcf2
milipili <milipili@shikami.org>
2009-05-06 20:58:19 +0000

    * Several fixes for compilation with MinGW  * Added the DevPack PThreads for MinGW
    git-svn-id: file:///home/michael/svn/yuni/trunk@478 7e728022-c0ab-4603-8e65-c666ead28a33

--- src/yuni/toolbox/system/sleep.cpp
+++ src/yuni/toolbox/system/sleep.cpp
@@ -1,6 +1,6 @@
 
 #include "sleep.h"
-#if defined(YUNI_OS_WINDOWS) && defined(YUNI_OS_MSVC)
+#if defined(YUNI_OS_WINDOWS)
 # include "windows.hdr.h"
 #else
 # include <unistd.h>
@@ -13,20 +13,20 @@ namespace Yuni
 
 	void Sleep(const uint32 seconds)
 	{
-		# if defined(YUNI_OS_WINDOWS) && defined(YUNI_OS_MSVC)
+		# if defined(YUNI_OS_WINDOWS)
 		::Sleep(1000 * seconds);
 		# else
-		sleep(seconds);
+		::sleep(seconds);
 		# endif
 	}
 
 
 	void SleepMilliSeconds(const uint32 milliseconds)
 	{
-		# if defined(YUNI_OS_WINDOWS) && defined(YUNI_OS_MSVC)
+		# if defined(YUNI_OS_WINDOWS)
 		::Sleep(milliseconds);
 		# else
-		usleep(1000 * milliseconds);
+		::usleep(1000 * milliseconds);
 		# endif
 	}
 
