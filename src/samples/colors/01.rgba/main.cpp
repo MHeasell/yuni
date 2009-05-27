56f39e1f5dfb5114de92b0a1124b8315bd623217
loomchild <loomchild@7e728022-c0ab-4603-8e65-c666ead28a33>
2009-05-27 13:23:49 +0000

    Minor typo/ortho fixes.
    git-svn-id: http://svn.ta3d.org/yuni/trunk@515 7e728022-c0ab-4603-8e65-c666ead28a33

--- src/samples/colors/01.rgba/main.cpp
+++ src/samples/colors/01.rgba/main.cpp
@@ -8,23 +8,23 @@ int main(void)
 {
 	// Only two types should be used : uint8 and float. The other ones are not recommended
 
-	// Standards colors when using an uint8.
+	// Standard colors when using an uint8.
 	// The limits are : 0..255 (`0` means 0%, `255` means 100%).
-	Gfx::Color::RGBA<uint8> b_red(   255, 0,   0,   255); // By default, a color is opaque
-	Gfx::Color::RGBA<uint8> b_green( 0,   255, 0);
+	Gfx::Color::RGBA<uint8> b_red(   255, 0,   0,   255);
+	Gfx::Color::RGBA<uint8> b_green( 0,   255, 0); // By default, a color is opaque
 	Gfx::Color::RGBA<uint8> b_blue(  0,   0,   255, 255);
 
 	// The same colors when using a float (OpenGL uses float for the colors)
 	// The limits are : 0..1 (`0.0f` means 0%, `1.0f` means 100%)
-	Gfx::Color::RGBA<float> f_red(   1.0f,  0.0f, 0.0f, 1.0f); // By default, a color is opaque
-	Gfx::Color::RGBA<float> f_green( 0.0f,  1.0f, 0.0f);
+	Gfx::Color::RGBA<float> f_red(   1.0f,  0.0f, 0.0f, 1.0f);
+	Gfx::Color::RGBA<float> f_green( 0.0f,  1.0f, 0.0f); // By default, a color is opaque
 	Gfx::Color::RGBA<float> f_blue(  0.0f,  0.0f, 1.0f, 1.0f);
 
 	// A color completely transparent
 	Gfx::Color::RGBA<float> f_transparent(0.3f, 0.72f, 0.2345f, 0.3f);
 	std::cout << "Transparent : " << f_transparent << std::endl;
 
-	// Convertions
+	// Conversions
 	Gfx::Color::RGBA<float> f_convert (Gfx::Color::RGBA<uint8>(242, 24, 183, 255));
 	std::cout << "Convert 1 : " << f_convert << std::endl;
 
