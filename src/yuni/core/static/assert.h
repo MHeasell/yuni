a109720c556cc52c0a12ffc8a307364921e90f9f
milipili <milipili@shikami.org>
2009-07-10 15:38:51 +0000

    Fixed the static assert
    git-svn-id: http://svn.ta3d.org/yuni/trunk@567 7e728022-c0ab-4603-8e65-c666ead28a33

--- src/yuni/core/static/assert.h
+++ src/yuni/core/static/assert.h
@@ -4,58 +4,94 @@
 # include "../preprocessor/std.h"
 
 
+/*!
+** \def YUNI_STATIC_ASSERT(X,ID)
+** \brief Assert at compile time
+**
+**
+** YUNI_STATIC_ASSERT is like assert for C/C++ (man 3 assert), except that the
+** test is done at compile time.
+**
+** Here is an example to produce an error to prevent compilation from a missing
+** implementation :
+** \code
+** #include <iostream>
+** #include <yuni/core/static/assert.h>
+**
+**
+** template<typename T>
+** struct Dummy
+** {
+** 	void foo()
+** 	{
+** 		YUNI_STATIC_ASSERT(false, TheImplementationIsMissing);
+** 	}
+** };
+**
+** template<>
+** struct Dummy<int>
+** {
+** 	void foo()
+** 	{
+** 		std::cout << "Called Dummy<int>::foo()" << std::endl;
+** 	}
+** };
+**
+** int main()
+** {
+** 	// As the static assert is within a method and not the class itself,
+** 	// it is possible to instanciate the class in all cases.
+** 	// But this is not true for the method `foo()`.
+**
+** 	Dummy<int> dummy0;
+** 	// The following statement will compile
+** 	dummy0.foo();
+**
+** 	Dummy<unsigned int> dummy1;
+** 	// But this one will failt
+** 	dummy1.foo();
+**
+** 	// Error with gcc 3.x :
+** 	// ./main.cpp: In member function `void Dummy<T>::foo() [with T = unsigned int]':
+** 	//	./main.cpp:36:   instantiated from here
+** 	//	./main.cpp:11: error: creating array with size zero (` Assert_TheImplementationIsMissing')
+**
+** 	// Error with gcc 4.x :
+** 	// ./main.cpp: In member function ‘void Dummy<T>::foo() [with T = unsigned int]’:
+** 	//	./main.cpp:36:   instantiated from here
+** 	//	./main.cpp:11: error: creating array with negative size (‘(StaticAssert_TheImplementationIsMissing::._67)-0x000000001’)
+** 	return 0;
+** }
+** \endcode
+**
+** This macro might be used anywhere.
+** <br />
+** When using several static asserts within the same scope, each ID must be
+** unique.
+**
+** \param X An expression
+** \param ID An unique ID for the scope
+*/
+# define YUNI_STATIC_ASSERT(X,ID)  \
+		struct StaticAssert_##ID { \
+			enum { Assert_##ID = Yuni::Static::Assert<(0 != (X))>::result }; \
+		}; \
+		typedef char invokeStaticAssert_##ID[StaticAssert_##ID::Assert_##ID]
+
+
+
 
 namespace Yuni
 {
 namespace Static
 {
 
+	template<int N> struct Assert   { enum {result =  1}; }; // No error
 
-	/*!
-	** \brief Compile time Error Definition
-	**
-	** \attention This class should not be used directly. Consieder the
-	** macro `YUNI_STATIC_ASSERT` instead.
-	** \see YUNI_STATIC_ASSERT()
-	*/
-	template<bool X, typename T> struct CompileTimeError;
-	template<typename T> struct CompileTimeError<true, T> { enum { Error = 1}; };
+	template<> struct Assert<false> { enum {result = -1}; }; // Error
 
 
 } // namespace Static
 } // namespaec Yuni
-
-
-
-/*!
-** \def YUNI_STATIC_ASSERT(X,ID)
-** \brief Assert at compile time
-**
-** YUNI_STATIC_ASSERT is like assert for C++, except that the test is done at compile time.
-** (only when the test is possible at compile time).
-**
-** Here is an example to produce an error when a code is
-** instancied by the compiler:
-** \code
-** class CanNotBeInstancied
-** {
-**		YUNI_STATIC_ASSERT(false, ThisClassMustNotBeInstancied);
-** };
-** // Will produce something like this :
-** // error: incomplete type ‘CompileTimeError<false, YuniStaticAssert_ThisClassMustNotBeInstancied::Failed>’ used in nested name specifier
-** \endcode
-**
-** This macro might be used anywhere.
-** This tool is used by the ownership policy `NoCopy`.
-**
-** \param X An expression
-** \param ID An unique ID for the current scope
-*/
-# define YUNI_STATIC_ASSERT(X,ID)  \
-		struct YUNI_JOIN(YuniStaticAssert_,ID) \
-		{ struct Failed; enum { value = Yuni::Static::CompileTimeError<(0 != (X)), Failed>::Error }; }
-
-
-
 
 #endif // __YUNI_CORE_STATIC_ASSERT_H__
