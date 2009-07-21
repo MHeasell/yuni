37c90ab601052612ff6b0ae993fa3df4c94f9670
loomchild <loomchild@7e728022-c0ab-4603-8e65-c666ead28a33>
2009-07-21 16:16:54 +0000

    Include fixes for GL under windows
    git-svn-id: file:///home/michael/svn/yuni/trunk@588 7e728022-c0ab-4603-8e65-c666ead28a33

--- src/yuni/private/gfx3d/gl/gl.h
+++ src/yuni/private/gfx3d/gl/gl.h
@@ -8,7 +8,11 @@
 #  include <OpenGL/glu.h>
 #  include <GLUT/glut.h>
 # else
-#  include <GL/glut.h>
+#  ifdef YUNI_OS_WINDOWS
+#   include "../../../core/system/windows.hdr.h"
+#  endif // YUNI_OS_WINDOWS
+#  include <GL/gl.h>
+#  include <GL/glu.h>
 # endif // YUNI_OS_MAC
 
 #endif // __YUNI_PRIVATE_GFX_OPENGL_H__
