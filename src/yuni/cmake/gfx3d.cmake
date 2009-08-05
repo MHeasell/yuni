047cac259eb516dff00277ed257c1c2817049984
milipili <milipili@shikami.org>
2009-08-05 20:14:34 +0000

    Gfx3D:  * Added 3 defines to avoid the compilation for all Window Systems   * YUNI_WINDOWSYSTEM_MSW   : For MFC or any Window System on the Microsoft Windows platform   * YUNI_WINDOWSYSTEM_X11   : For all Unixes/Linuxes   * YUNI_WINDOWSYSTEM_COCOA : For Mac OS X  * Renammed some .hxx files into .cpp  * Yuni Coding Style
    git-svn-id: file:///home/michael/svn/yuni/trunk@634 7e728022-c0ab-4603-8e65-c666ead28a33

--- src/yuni/cmake/gfx3d.cmake
+++ src/yuni/cmake/gfx3d.cmake
@@ -6,19 +6,25 @@ Set(SRC_GFX3D
 		gfx/window/window.h
 		gfx/window/factory.h gfx/window/factory.cpp
 		gfx/window/opengl.h gfx/window/opengl.cpp
-		gfx/window/openglmsw.h gfx/window/openglmsw.hxx
-		gfx/window/openglx11.h gfx/window/openglx11.cpp
+		gfx/window/openglmsw.h
+		gfx/window/openglx11.h
 		gfx/window/x11yuni.h
 		)
+
+IF(WIN32)
+	Set(SRC_GFX3D ${SRC_GFX3D} gfx/window/openglmsw.cpp)
+ELSE(WIN32)
+	IF(APPLE)
+	ELSE(APPLE)
+		Set(SRC_GFX3D ${SRC_GFX3D} gfx/window/openglx11.cpp)
+	ENDIF(APPLE)
+ENDIF(WIN32)
+
 source_group(Gfx3D FILES ${SRC_GFX3D})
 
 
 
-Add_Library(yuni-static-gfx3d-core STATIC
-		${SRC_GFX3D}
-	   )
-
-
+Add_Library(yuni-static-gfx3d-core STATIC ${SRC_GFX3D})
 
 
 
