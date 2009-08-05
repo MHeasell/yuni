b4a13fdb954e6a7064356c9c4926f06c5385cbba
milipili <milipili@shikami.org>
2009-08-05 19:45:45 +0000

    Missing file
    git-svn-id: file:///home/michael/svn/yuni/trunk@632 7e728022-c0ab-4603-8e65-c666ead28a33

---
+++ src/yuni/gfx/window/x11yuni.h
@@ -0,0 +1,16 @@
+#ifndef __YUNI_GFX_WINDOW_X11_YUNI_H__
+# define __YUNI_GFX_WINDOW_X11_YUNI_H__
+
+
+# include "../../yuni.h"
+# include <GL/glx.h>
+
+// The define None from the X11 headers overlaps with Yuni::None...
+# ifdef None
+#	undef None
+# endif
+
+# define YUNI_X11LIB_NONE  0L
+
+
+#endif // __YUNI_GFX_WINDOW_X11_YUNI_H__
