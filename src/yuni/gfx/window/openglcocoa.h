2db3d8d3665a57e8e901d2463e5671b330f9f111
loomchild <loomchild@7e728022-c0ab-4603-8e65-c666ead28a33>
2009-08-11 11:57:42 +0000

    Added missing file.
    git-svn-id: http://svn.ta3d.org/yuni/trunk@660 7e728022-c0ab-4603-8e65-c666ead28a33

---
+++ src/yuni/gfx/window/openglcocoa.h
@@ -0,0 +1,39 @@
+#ifndef __YUNI_GFX_WINDOW_OPENGL_COCOA_H__
+# define __YUNI_GFX_WINDOW_OPENGL_COCOA_H__
+
+# include "../../yuni.h"
+# include "opengl.h"
+
+
+namespace Yuni
+{
+namespace Gfx3D
+{
+namespace Window
+{
+
+	class OpenGLCocoa: public OpenGL
+	{
+	public:
+		OpenGLCocoa(const String& title, unsigned int width, unsigned int height, unsigned int bitDepth, bool fullScreen)
+			:OpenGL(title, width, height, bitDepth, fullScreen)
+		{}
+
+		virtual bool initialize() { return OpenGL::initialize(); }
+		virtual void close() { OpenGL::close(); }
+		virtual void blit() {}
+		// virtual bool pollEvents();
+
+		virtual void onTitleChanged();
+
+	}; // class OpenGLCocoa
+
+
+
+
+} // namespace Window
+} // namespace Gfx3D
+} // namespace Yuni
+
+
+#endif // __YUNI_GFX_WINDOW_OPENGL_COCOA_H__
