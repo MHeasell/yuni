7085cde4ad5fe495d03a2cb0492025c43fb57884
milipili <milipili@shikami.org>
2009-09-22 08:10:19 +0000

    libyuni-config: Fixed compilation on Windows (PThread _must_ not be used)
    git-svn-id: http://svn.ta3d.org/yuni/trunk@762 7e728022-c0ab-4603-8e65-c666ead28a33

--- src/tools/libyuni-config/main.cpp
+++ src/tools/libyuni-config/main.cpp
@@ -1,5 +1,8 @@
 
-#define YUNI_NO_THREAD_SAFE // disabling thread-safety
+#ifndef YUNI_NO_THREAD_SAFE
+#	define YUNI_NO_THREAD_SAFE // disabling thread-safety
+#endif
+#include <yuni/yuni.h>
 #include "program.h"
 
 
