b330a3a04d4c8dfa6e988ad2f8e00ac09054e200
milipili <milipili@shikami.org>
2009-12-18 19:50:22 +0000

    * Added routines for efficiently dealing with variadic macros  * Added routines for various macro expansion
    git-svn-id: http://svn.ta3d.org/yuni/trunk@888 7e728022-c0ab-4603-8e65-c666ead28a33

--- src/yuni/core/preprocessor/std.h
+++ src/yuni/core/preprocessor/std.h
@@ -11,5 +11,24 @@
 # define YUNI_UNUSED_ARGUMENT(X) (void)(X)
 
 
+/*! The identity function */
+# define YUNI_IDENTITY(...) __VA_ARGS__
+
+/*! An empty value */
+# define YUNI_EMPTY
+
+/*! Comma */
+# define YUNI_COMMA ,
+
+/*! Semicolon */
+# define YUNI_SEMICOLON ;
+
+/*! Dot */
+# define YUNI_DOT .
+
+/*! Minus */
+# define YUNI_MINUS -
+
+
 
 #endif /* __YUNI_CORE_PREPROCESSOR_STANDARD_H__ */
