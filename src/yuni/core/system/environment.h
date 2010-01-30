48816d0916d5891799e8a254754d4ec163d70ee3
milipili <milipili@shikami.org>
2010-01-30 22:42:59 +0000

    System::Environment:  * Fixed compilation (the namespace `Yuni::Core::Traits` had been renamed into `Yuni::Traits`)  * The variable given to `System::Environment::Read` may not be emptied before appending the value of an environment variable.
    git-svn-id: http://svn.ta3d.org/yuni/trunk@967 7e728022-c0ab-4603-8e65-c666ead28a33

--- src/yuni/core/system/environment.h
+++ src/yuni/core/system/environment.h
@@ -24,10 +24,17 @@ namespace Environment
 	** \brief Get a value from the current environment
 	**
 	** \param name Name of the variable
-	** \param out Variable of type 'string'/'container' where to write the value
+	** \param out Variable of type 'string'/'container' where the value will be happened
+	** \param emptyBefore True to empty the parameter `out` before. We may not want this
+	**   behavior to directly append the value of an environment variable to an exising
+	**   buffer.
 	** \return True if the operation succeeded, false otherwise
 	*/
-	template<class AnyStringT, class AnyStringT2> bool Read(const AnyStringT& name, AnyStringT2& out);
+	template<class AnyStringT, class AnyStringT2>
+	bool Read(const AnyStringT& name, AnyStringT2& out, const bool emptyBefore = true);
+
+
+
 
 
 } // namespace Environment
