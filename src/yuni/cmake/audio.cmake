be2b42dfa6273d903634c288c240d25c3207ad70
loomchild <loomchild@7e728022-c0ab-4603-8e65-c666ead28a33>
2010-04-13 20:43:08 +0000

    Updated cmake stuff for ffmpeg needs
    git-svn-id: file:///home/michael/svn/yuni/trunk@1010 7e728022-c0ab-4603-8e65-c666ead28a33

--- src/yuni/cmake/audio.cmake
+++ src/yuni/cmake/audio.cmake
@@ -8,13 +8,6 @@ LIBYUNI_CONFIG_DEPENDENCY("audio" "core") # yuni-core is required
 LIBYUNI_CONFIG_DEPENDENCY("audio" "gfx") # gfx-core is required
 # LIBYUNI_CONFIG_LIB("audio"  "yuni-static-device-audio")
 
-IF(APPLE)
-	# Frameworks
-	LIBYUNI_CONFIG_FRAMEWORK("audio" OpenAL)
-ENDIF(APPLE)
-
-
-
 
 Set(SRC_AUDIO
 #		audio/sound.h
@@ -46,21 +39,37 @@ Include(CheckIncludeFile)
 # OpenAL
 #
 Message(STATUS "Added Support for OpenAL")
-DEVPACK_IMPORT_OPENAL()
-LIBYUNI_CONFIG_LIB_RAW_COMMAND("audio" "${OPENAL_LIBRARY}")
-LIBYUNI_CONFIG_INCLUDE_PATH("audio" "${OPENAL_INCLUDE_DIR}")
+IF(APPLE)
+	# Frameworks
+	LIBYUNI_CONFIG_FRAMEWORK("audio" OpenAL)
+ELSE(APPLE)
+	DEVPACK_IMPORT_OPENAL()
+	LIBYUNI_CONFIG_LIB_RAW_COMMAND("audio" "${OPENAL_LIBRARY}")
+	LIBYUNI_CONFIG_INCLUDE_PATH("audio" "${OPENAL_INCLUDE_DIR}")
+ENDIF(APPLE)
 
 #
 # FFmpeg
 #
 Message(STATUS "Added Support for FFMpeg")
 DEVPACK_IMPORT_FFMPEG()
+SET(SRC_AUDIO_FFMPEG ${YUNI_EXT_FFMPEG_HEADERS})
+IF (ZLIB_FOUND)
+	IF(BZIP2_FOUND)
+		LIST(APPEND YUNI_EXT_FFMPEG_LIB ${ZLIB_LIBRARIES} ${BZIP2_LIBRARIES})
+	ELSE(BZIP2_FOUND)
+		MESSAGE(STATUS "BZip2 library is missing! Audio module will not work properly!")
+	ENDIF(BZIP2_FOUND)
+ELSE (ZLIB_FOUND)
+	MESSAGE(STATUS "Zip library is missing! Audio module will not work properly!")
+ENDIF (ZLIB_FOUND)
 LIBYUNI_CONFIG_LIB_RAW_COMMAND("audio" "${YUNI_EXT_FFMPEG_LIB}")
 LIBYUNI_CONFIG_INCLUDE_PATH("audio" "${YUNI_EXT_FFMPEG_INCLUDE}")
 
 
 source_group(Audio FILES ${SRC_AUDIO})
+source_group(Audio\\Ffmpeg FILES ${SRC_AUDIO_FFMPEG})
 
-Add_Library(yuni-static-audio-core STATIC ${SRC_AUDIO})
+Add_Library(yuni-static-audio-core STATIC ${SRC_AUDIO_FFMPEG} ${SRC_AUDIO})
 
 target_link_libraries(yuni-static-audio-core ${OPENAL_LIBRARY})
