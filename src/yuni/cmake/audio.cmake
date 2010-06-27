bba8641c6b46fe85f45df82f5b8860c3a7816083
milipili <milipili@shikami.org>
2010-06-27 13:26:47 +0000

    Audio: fixed compilation on Linux (#238)
    git-svn-id: http://svn.ta3d.org/yuni/trunk@1130 7e728022-c0ab-4603-8e65-c666ead28a33

--- src/yuni/cmake/audio.cmake
+++ src/yuni/cmake/audio.cmake
@@ -1,24 +1,16 @@
 
 Message(STATUS ":: [Module] Audio")
 
-IF (NOT WIN32 AND NOT WIN64)
-   FIND_PACKAGE(ZLIB)
-   FIND_PACKAGE(BZip2)
-ENDIF (NOT WIN32 AND NOT WIN64)
-
 
 LIBYUNI_CONFIG_LIB("audio"      "yuni-static-audio-core")
-
 LIBYUNI_CONFIG_DEPENDENCY("audio" "core") # yuni-core is required
-LIBYUNI_CONFIG_DEPENDENCY("audio" "gfx") # gfx-core is required
-# LIBYUNI_CONFIG_LIB("audio"  "yuni-static-device-audio")
 
 
 #
 # Windows-specific
 #
 IF (WIN32 OR WIN64)
-	LIBYUNI_CONFIG_LIB_RAW_COMMAND("audio" "-lws2_32")
+	LIBYUNI_CONFIG_LIB("audio" "ws2_32")
 ENDIF (WIN32 OR WIN64)
 
 
@@ -28,7 +20,6 @@ Set(SRC_AUDIO
 		audio/queueservice.cpp
 		audio/loop.h
 		audio/loop.cpp
-#		audio/playlist.h
 		audio/emitter.h
 		audio/emitter.hxx
 		audio/emitter.cpp
@@ -53,9 +44,19 @@ IF(APPLE)
 	# Frameworks
 	LIBYUNI_CONFIG_FRAMEWORK("audio" OpenAL)
 ELSE(APPLE)
-	DEVPACK_IMPORT_OPENAL()
+	IF(WIN32 OR WIN64)
+		DEVPACK_IMPORT_OPENAL()
+	ELSE(WIN32 OR WIN64)
+		FIND_PACKAGE(OpenAL)
+		IF(NOT OPENAL_FOUND)
+			Message(SEND_ERROR "[!!] Impossible to find OpenAL")
+			Message(STATUS     " * Packages needed on Debian: libopenal-dev")
+			Message(STATUS     " * Packages needed on Fedora: openal-devel")
+		ENDIF(NOT OPENAL_FOUND)
+	ENDIF(WIN32 OR WIN64)
 	LIBYUNI_CONFIG_LIB_RAW_COMMAND("audio" "${OPENAL_LIBRARY}")
 	LIBYUNI_CONFIG_INCLUDE_PATH("audio" "${OPENAL_INCLUDE_DIR}")
+	Include_directories(${OPENAL_INCLUDE_DIR})
 ENDIF(APPLE)
 
 #
@@ -64,15 +65,29 @@ ENDIF(APPLE)
 Message(STATUS "Added Support for FFMpeg")
 DEVPACK_IMPORT_FFMPEG()
 SET(SRC_AUDIO_FFMPEG ${YUNI_EXT_FFMPEG_HEADERS})
-IF (ZLIB_FOUND)
+
+IF (NOT WIN32 AND NOT WIN64)
+	# ZLIB
+	FIND_PACKAGE(ZLIB)
+	IF (ZLIB_FOUND)
+		LIST(APPEND YUNI_EXT_FFMPEG_LIB ${ZLIB_LIBRARIES})
+	ELSE (ZLIB_FOUND)
+		Message(STATUS     "[!!] Impossible to find ZLib (Audio will not work properly !)")
+		Message(STATUS     " * Packages needed on Debian: libz-dev")
+		Message(STATUS     " * Packages needed on Fedora: zlib-devel")
+	ENDIF (ZLIB_FOUND)
+
+	# BZIP2
+	FIND_PACKAGE(BZip2)
 	IF(BZIP2_FOUND)
-		LIST(APPEND YUNI_EXT_FFMPEG_LIB ${ZLIB_LIBRARIES} ${BZIP2_LIBRARIES})
+		LIST(APPEND YUNI_EXT_FFMPEG_LIB ${BZIP2_LIBRARIES})
 	ELSE(BZIP2_FOUND)
-		MESSAGE(STATUS "BZip2 library is missing! Audio module will not work properly!")
+		Message(STATUS     "[!!] Impossible to find BZip2 (Audio will not work properly !)")
+		Message(STATUS     " * Packages needed on Debian: libbz2-dev")
+		Message(STATUS     " * Packages needed on Fedora: bzip2-devel")
 	ENDIF(BZIP2_FOUND)
-ELSE (ZLIB_FOUND)
-	MESSAGE(STATUS "Zip library is missing! Audio module will not work properly!")
-ENDIF (ZLIB_FOUND)
+ENDIF (NOT WIN32 AND NOT WIN64)
+
 LIBYUNI_CONFIG_LIB_RAW_COMMAND("audio" "${YUNI_EXT_FFMPEG_LIB}")
 LIBYUNI_CONFIG_INCLUDE_PATH("audio" "${YUNI_EXT_FFMPEG_INCLUDE}")
 
