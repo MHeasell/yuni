81883d4bfa90a8b98d2a362c8c319515da60bf23
footy <footy@7e728022-c0ab-4603-8e65-c666ead28a33>
2010-07-01 20:55:56 +0000

    * Finished transition libyuni-config => yuni-config in CMakeLists.txt. See #241 and r1134.
    git-svn-id: http://svn.ta3d.org/yuni/trunk@1142 7e728022-c0ab-4603-8e65-c666ead28a33

--- packages/cmake/FindYuni.cmake
+++ packages/cmake/FindYuni.cmake
@@ -2,18 +2,18 @@
 # Locate the Yuni Framework
 #
 # This module defines :
-#  - LibYuni_FOUND    : If the libraries has been found
-#  - LibYuni_Info     : A string about the selected version of yuni (`Not found` by default)
-#  - LibYuni_CXXFLAGS : The flags to use for compiling a source file
-#  - LibYuni_LIBS     : The flags to use to link against the libyuni libraries
-#  - LibYuni_Config   : Full path to the program 'yuni-config' (empty if not found)
+#  - Yuni_FOUND    : If the libraries has been found
+#  - Yuni_Info     : A string about the selected version of yuni (`Not found` by default)
+#  - Yuni_CXXFLAGS : The flags to use for compiling a source file
+#  - Yuni_LIBS     : The flags to use to link against the libyuni libraries
+#  - Yuni_Config   : Full path to the program 'yuni-config' (empty if not found)
 #
 # Usage :
 # \code
-# find_package(LibYuni COMPONENTS core gfx3d lua)
-# if(LibYuni_NOTFOUND)
+# find_package(Yuni COMPONENTS core gfx3d lua)
+# if(Yuni_NOTFOUND)
 #	message(ERROR "The yuni framework could not been found.")
-# endif(LibYuni_NOTFOUND)
+# endif(Yuni_NOTFOUND)
 # \endcode
 #
 
@@ -21,10 +21,10 @@
 #
 # Init
 #
-SET(LibYuni_CXXFLAGS "")
-SET(LibYuni_LIBS     "")
-SET(LibYuni_INFOS    "Not found.")
-SET(LibYuni_Config   "")
+SET(Yuni_CXXFLAGS "")
+SET(Yuni_LIBS     "")
+SET(Yuni_INFOS    "Not found.")
+SET(Yuni_Config   "")
 
 
 
@@ -34,70 +34,70 @@ SET(LibYuni_Config   "")
 
 
 
-SET(__LibYuni_Message "Looking for the Yuni Framework")
+SET(__Yuni_Message "Looking for the Yuni Framework")
 
 #
 # Where is yuni-config ?
 #
-SET(__LibYuni_CurrentFolder "${CMAKE_CURRENT_LIST_FILE}")
-STRING(REPLACE "\\FindYuni.cmake" "" __LibYuni_CurrentFolder "${__LibYuni_CurrentFolder}")
-STRING(REPLACE "/FindYuni.cmake" "" __LibYuni_CurrentFolder "${__LibYuni_CurrentFolder}")
+SET(__Yuni_CurrentFolder "${CMAKE_CURRENT_LIST_FILE}")
+STRING(REPLACE "\\FindYuni.cmake" "" __Yuni_CurrentFolder "${__Yuni_CurrentFolder}")
+STRING(REPLACE "/FindYuni.cmake" "" __Yuni_CurrentFolder "${__Yuni_CurrentFolder}")
 IF(WIN32 OR WIN64)
-	SET(__LibYuni_ProgramSearchPath
+	SET(__Yuni_ProgramSearchPath
 		C:/dev/libyuni
 		C:/libyuni
-		"${__LibYuni_CurrentFolder}/../../src/tools/yuni-config"
-		"${__LibYuni_CurrentFolder}/../../src/tools/yuni-config/Release"
-		"${__LibYuni_CurrentFolder}/../../src/tools/yuni-config/Debug"
-		"$ENV{LibYuniPaths}")
+		"${__Yuni_CurrentFolder}/../../src/tools/yuni-config"
+		"${__Yuni_CurrentFolder}/../../src/tools/yuni-config/Release"
+		"${__Yuni_CurrentFolder}/../../src/tools/yuni-config/Debug"
+		"$ENV{YuniPaths}")
 ELSE(WIN32 OR WIN64)
-	SET(__LibYuni_ProgramSearchPath
-		"${__LibYuni_CurrentFolder}/../../src/tools/yuni-config" "$ENV{LibYuniPaths}")
+	SET(__Yuni_ProgramSearchPath
+		"${__Yuni_CurrentFolder}/../../src/tools/yuni-config" "$ENV{YuniPaths}")
 ENDIF(WIN32 OR WIN64)
 
-FIND_PROGRAM(__LibYuni_Config NAMES yuni-config yuni-config.exe
-    PATHS ${__LibYuni_ProgramSearchPath})
-IF("${__LibYuni_Config}" STREQUAL "__LibYuni_Config-NOTFOUND")
-	MESSAGE(STATUS "${__LibYuni_Message} - failed ('yuni-config' not found)")
-	SET(LibYuni_NOTFOUND TRUE)
-ENDIF("${__LibYuni_Config}" STREQUAL "__LibYuni_Config-NOTFOUND")
+FIND_PROGRAM(__Yuni_Config NAMES yuni-config yuni-config.exe
+    PATHS ${__Yuni_ProgramSearchPath})
+IF("${__Yuni_Config}" STREQUAL "__Yuni_Config-NOTFOUND")
+	MESSAGE(STATUS "${__Yuni_Message} - failed ('yuni-config' not found)")
+	SET(Yuni_NOTFOUND TRUE)
+ENDIF("${__Yuni_Config}" STREQUAL "__Yuni_Config-NOTFOUND")
 
 
-IF(NOT LibYuni_NOTFOUND)
+IF(NOT Yuni_NOTFOUND)
 
 	# gotcha !
-	SET(LibYuni_Config   "${__LibYuni_Config}")
+	SET(Yuni_Config   "${__Yuni_Config}")
 
 	#
 	# Compiler
 	#
-	SET(__LibYuni_Compiler "gcc")
+	SET(__Yuni_Compiler "gcc")
 	IF(MINGW)
-		SET(__LibYuni_Compiler "mingw")
+		SET(__Yuni_Compiler "mingw")
 	ENDIF(MINGW)
 	IF(MSVC)
-		SET(__LibYuni_Compiler "msvc")
+		SET(__Yuni_Compiler "msvc")
 	ENDIF(MSVC)
 	IF(WATCOM)
-		SET(__LibYuni_Compiler "watcom")
+		SET(__Yuni_Compiler "watcom")
 	ENDIF(WATCOM)
 	IF(BORLAND)
-		SET(__LibYuni_Compiler "borland")
+		SET(__Yuni_Compiler "borland")
 	ENDIF(BORLAND)
 	IF(MSYS)
-		SET(__LibYuni_Compiler "mingw")
+		SET(__Yuni_Compiler "mingw")
 	ENDIF(MSYS)
 	IF(CYGWIN)
-		SET(__LibYuni_Compiler "mingw")
+		SET(__Yuni_Compiler "mingw")
 	ENDIF(CYGWIN)
 
 	#
 	# Building the command line options for the list of components
 	#
-	SET(__LibYuni_ModsMods "")
-	FOREACH(COMPONENT ${LibYuni_FIND_COMPONENTS})
+	SET(__Yuni_ModsMods "")
+	FOREACH(COMPONENT ${Yuni_FIND_COMPONENTS})
 		STRING(TOLOWER ${COMPONENT} COMPONENT)
-		SET(__LibYuni_ModsOpts "${__LibYuni_ModsOpts} ${COMPONENT}")
+		SET(__Yuni_ModsOpts "${__Yuni_ModsOpts} ${COMPONENT}")
 	ENDFOREACH(COMPONENT)
 
 	#
@@ -106,38 +106,38 @@ IF(NOT LibYuni_NOTFOUND)
     IF(WIN32)
         IF(NOT MSYS)
             # On Windows (not MSys), a patch like this "C:/path/..." is invalid
-            STRING(REPLACE "/" "\\" __LibYuni_Config "${__LibYuni_Config}")
+            STRING(REPLACE "/" "\\" __Yuni_Config "${__Yuni_Config}")
         ENDIF(NOT MSYS)
     ENDIF(WIN32)
-	execute_process(COMMAND "${__LibYuni_Config}" -c "${__LibYuni_Compiler}"
-		-m "${__LibYuni_ModsOpts}" --module-deps
-		OUTPUT_STRIP_TRAILING_WHITESPACE OUTPUT_VARIABLE __LibYuni_ModsDeps
-		RESULT_VARIABLE __LibYuni_Config_DepsResult)
-	IF(NOT "${__LibYuni_Config_DepsResult}" EQUAL 0 OR "${__LibYuni_ModsDeps}" STREQUAL "")
-		MESSAGE(STATUS "${__LibYuni_Message} - Requires: ${LibYuni_FIND_COMPONENTS}")
-		MESSAGE(STATUS "${__LibYuni_Message} - failed - the required modules could not be found")
-		SET(LibYuni_NOTFOUND TRUE)
-	ENDIF(NOT "${__LibYuni_Config_DepsResult}" EQUAL 0 OR "${__LibYuni_ModsDeps}" STREQUAL "")
+	execute_process(COMMAND "${__Yuni_Config}" -c "${__Yuni_Compiler}"
+		-m "${__Yuni_ModsOpts}" --module-deps
+		OUTPUT_STRIP_TRAILING_WHITESPACE OUTPUT_VARIABLE __Yuni_ModsDeps
+		RESULT_VARIABLE __Yuni_Config_DepsResult)
+	IF(NOT "${__Yuni_Config_DepsResult}" EQUAL 0 OR "${__Yuni_ModsDeps}" STREQUAL "")
+		MESSAGE(STATUS "${__Yuni_Message} - Requires: ${Yuni_FIND_COMPONENTS}")
+		MESSAGE(STATUS "${__Yuni_Message} - failed - the required modules could not be found")
+		SET(Yuni_NOTFOUND TRUE)
+	ENDIF(NOT "${__Yuni_Config_DepsResult}" EQUAL 0 OR "${__Yuni_ModsDeps}" STREQUAL "")
 
-	IF(NOT LibYuni_NOTFOUND)
+	IF(NOT Yuni_NOTFOUND)
 		# Infos
-		execute_process(COMMAND "${__LibYuni_Config}" -c "${__LibYuni_Compiler}"
-			-m "${__LibYuni_ModsOpts}" -l
-			OUTPUT_STRIP_TRAILING_WHITESPACE OUTPUT_VARIABLE LibYuni_INFOS)
+		execute_process(COMMAND "${__Yuni_Config}" -c "${__Yuni_Compiler}"
+			-m "${__Yuni_ModsOpts}" -l
+			OUTPUT_STRIP_TRAILING_WHITESPACE OUTPUT_VARIABLE Yuni_INFOS)
 
 		# CXX
-		execute_process(COMMAND "${__LibYuni_Config}" -c "${__LibYuni_Compiler}"
-			-m "${__LibYuni_ModsOpts}" --cxxflags
-			OUTPUT_STRIP_TRAILING_WHITESPACE OUTPUT_VARIABLE LibYuni_CXXFLAGS
-			RESULT_VARIABLE __LibYuni_Config_DepsResult)
+		execute_process(COMMAND "${__Yuni_Config}" -c "${__Yuni_Compiler}"
+			-m "${__Yuni_ModsOpts}" --cxxflags
+			OUTPUT_STRIP_TRAILING_WHITESPACE OUTPUT_VARIABLE Yuni_CXXFLAGS
+			RESULT_VARIABLE __Yuni_Config_DepsResult)
 		# LIBS
-		execute_process(COMMAND "${__LibYuni_Config}" -c "${__LibYuni_Compiler}"
-			-m "${__LibYuni_ModsOpts}" --libs
-			OUTPUT_STRIP_TRAILING_WHITESPACE OUTPUT_VARIABLE LibYuni_LIBS
-			RESULT_VARIABLE __LibYuni_Config_DepsResult)
+		execute_process(COMMAND "${__Yuni_Config}" -c "${__Yuni_Compiler}"
+			-m "${__Yuni_ModsOpts}" --libs
+			OUTPUT_STRIP_TRAILING_WHITESPACE OUTPUT_VARIABLE Yuni_LIBS
+			RESULT_VARIABLE __Yuni_Config_DepsResult)
 
-		SET(LibYuni_FOUND true)
-	ENDIF(NOT LibYuni_NOTFOUND)
+		SET(Yuni_FOUND true)
+	ENDIF(NOT Yuni_NOTFOUND)
 
-ENDIF(NOT LibYuni_NOTFOUND)
+ENDIF(NOT Yuni_NOTFOUND)
 
