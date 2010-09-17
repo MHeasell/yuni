9f1bb7fa50583b5b14b395925ad8e457984f9381
loomchild <loomchild@7e728022-c0ab-4603-8e65-c666ead28a33>
2010-09-17 13:58:58 +0000

    Reorganized files in gfx.
    git-svn-id: file:///home/michael/svn/yuni/trunk@1240 7e728022-c0ab-4603-8e65-c666ead28a33

--- src/yuni/gfx/window/msw/cairo.cpp
+++ src/yuni/gfx/window/msw/cairo.cpp
@@ -11,7 +11,7 @@ namespace Window
 
 	inline bool Cairo::initialize()
 	{
-		 = cairo_win32_surface_create_with_ddb(GetDC(pHWND),
+		pSurface = cairo_win32_surface_create_with_ddb(GetDC(pHWND),
 			cairo_format_t.CAIRO_FORMAT_ARGB32, pWidth, pHeight);
 	}
 
