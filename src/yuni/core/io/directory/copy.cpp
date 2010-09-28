3cf3fa7c21a568cc8be32620938f78dedb972d2a
milipili <milipili@shikami.org>
2010-09-28 19:24:40 +0000

    Applied patch from #292 (Core::Directory::Copy)
    git-svn-id: file:///home/michael/svn/yuni/trunk@1262 7e728022-c0ab-4603-8e65-c666ead28a33

--- src/yuni/core/io/directory/copy.cpp
+++ src/yuni/core/io/directory/copy.cpp
@@ -39,9 +39,12 @@ namespace Directory
 	{
 		// normalize paths
 		String fsrc(src, srclen);
-		Yuni::Core::IO::Normalize(fsrc, src, (unsigned int)srclen);
+		Yuni::Core::IO::Normalize(fsrc, src, srclen);
+		if (fsrc.empty())
+			return false;
+
 		String fdst(dst, dstlen);
-		Yuni::Core::IO::Normalize(fdst, dst, (unsigned int)dstlen);
+		Yuni::Core::IO::Normalize(fdst, dst, dstlen);
 
 		// The list of files to copy
 		List list;
@@ -53,11 +56,12 @@ namespace Directory
 			InfoItem& info = list.back();
 			info.filename = fdst;
 			info.isFile   = false;
+			if (!onUpdate(Yuni::Core::IO::Directory::cpsGatheringInformation, fdst, fdst, 0, list.size()))
+				return false;
 		}
 
 		{
 			Yuni::Core::IO::Directory::Info info(fsrc);
-			unsigned int count = 0;
 			if (recursive)
 			{
 				const Yuni::Core::IO::Directory::Info::recursive_iterator end = info.recursive_end();
@@ -68,12 +72,8 @@ namespace Directory
 					info.filename = i.filename();
 					info.isFile   = i.isFile();
 					totalSize += i.size();
-					if (++count >> 5)
-					{
-						if (!onUpdate(Yuni::Core::IO::Directory::cpsGatheringInformation, *i, *i, 0, list.size()))
-							return false;
-						count = 0;
-					}
+					if (!onUpdate(Yuni::Core::IO::Directory::cpsGatheringInformation, *i, *i, 0, list.size()))
+						return false;
 				}
 			}
 			else
@@ -87,12 +87,8 @@ namespace Directory
 					info.isFile   = i.isFile();
 					totalSize += i.size();
 
-					if (++count >> 5)
-					{
-						if (!onUpdate(Yuni::Core::IO::Directory::cpsGatheringInformation, *i, *i, 0, list.size()))
-							return false;
-						count = 0;
-					}
+					if (!onUpdate(Yuni::Core::IO::Directory::cpsGatheringInformation, i.filename(), i.filename(), 0, list.size()))
+						return false;
 				}
 			}
 		}
@@ -101,33 +97,36 @@ namespace Directory
 		// A temporary string
 		String tmp;
 
+		// Streams : in the worst scenario, the last file to copy will be closed
+		// at the end of this routine
+		// Stream on the source file
 		Yuni::Core::IO::File::Stream fromFile;
+		// Stream on the target file
 		Yuni::Core::IO::File::Stream toFile;
-		enum { bufferSize = 8192 };
-		char* buffer = new char[bufferSize];
-		size_t numRead;
 
-		unsigned int count = 0;
+		// A temporary buffer for copying files' contents
+		// 16k seems to be a good choice (better than smaller block size with used
+		// in Virtual Machines)
+		enum { bufferSize = 16384 };
+		char* buffer = new char[bufferSize];
+
 		const List::const_iterator end = list.end();
 		for (List::const_iterator i = list.begin(); i != end; ++i)
 		{
+			// alias to the current information block
 			const InfoItem& info = *i;
-			tmp.clear() << fdst << Yuni::Core::IO::Separator;
-			tmp.append(info.filename.c_str() + fsrc.size() + 1, info.filename.size() - fsrc.size() - 1);
+
+			// Address of the target file
+			tmp = fdst; // without any OS-dependant separator
+			if (fsrc.size() < info.filename.size())
+				tmp.append(info.filename.c_str() + fsrc.size(), info.filename.size() - fsrc.size());
 
 			if (!info.isFile)
 			{
-				if (++count >> 5)
-				{
-					if (!onUpdate(Yuni::Core::IO::Directory::cpsCopying, info.filename, tmp, current, totalSize))
-					{
-						delete[] buffer;
-						return false;
-					}
-				}
-				count = 0;
-
-				if (!Yuni::Core::IO::Directory::Create(tmp))
+				// The target file is actually a folder
+				// We have to create it before copying its content
+				if (!onUpdate(Yuni::Core::IO::Directory::cpsCopying, info.filename, tmp, current, totalSize)
+					|| !Yuni::Core::IO::Directory::Create(tmp))
 				{
 					delete[] buffer;
 					return false;
@@ -135,34 +134,34 @@ namespace Directory
 			}
 			else
 			{
+				// The target file is a real file (and not a folder)
+				// Checking first for overwritting
 				if (!overwrite && Yuni::Core::IO::Exists(tmp))
 					continue;
 
-				// Open the source file
+				// Try to open the source file
+				// The previous opened source file will be closed here
 				if (fromFile.open(info.filename, Yuni::Core::IO::OpenMode::read))
 				{
+					// Try to open for writing the target file
+					// The previous opened target file will be closed here
 					if (toFile.open(tmp, Yuni::Core::IO::OpenMode::write | Yuni::Core::IO::OpenMode::truncate))
 					{
+						// reading the whole source file
+						size_t numRead;
 						while ((numRead = fromFile.read(buffer, bufferSize)) != 0)
 						{
+							// progression
 							current += numRead;
-							if (numRead != toFile.write((const char*)buffer, numRead))
+
+							// Trying to copy the block which has just been read
+							if (numRead != toFile.write((const char*)buffer, numRead)
+								|| !onUpdate(Yuni::Core::IO::Directory::cpsCopying, info.filename, tmp, current, totalSize))
 							{
 								delete[] buffer;
 								return false;
 							}
-
-							if (++count >> 7)
-							{
-								if (!onUpdate(Yuni::Core::IO::Directory::cpsCopying, info.filename, tmp, current, totalSize))
-								{
-									delete[] buffer;
-									return false;
-								}
-
-								count = 0;
-							}
-						}
+						} // read
 					}
 				}
 			}
