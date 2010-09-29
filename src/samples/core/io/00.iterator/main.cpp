ffdc4643e6fa12bf1e3dfae2673becea5774debd
loomchild <loomchild@7e728022-c0ab-4603-8e65-c666ead28a33>
2010-09-29 09:51:24 +0000

    Added comments in the directory iterator sample.
    git-svn-id: http://svn.ta3d.org/yuni/trunk@1272 7e728022-c0ab-4603-8e65-c666ead28a33

--- src/samples/core/io/00.iterator/main.cpp
+++ src/samples/core/io/00.iterator/main.cpp
@@ -19,18 +19,28 @@ int main(int argc, char** argv)
 	{
 		// Get directory information
 		Core::IO::Directory::Info info(argv[i]);
+		// Verify that the given path exists as a directory on the file system
 		if (!info.exists())
 		{
 			std::cerr << "Invalid directory : \"" << argv[i] << "\"" << std::endl;
+			// Ignore this path
 			continue;
 		}
+
+		// Use a recursive iterator. It is used exactly as an STL iterator
+		// All available iterators are :
+		// * iterator, file_iterator, folder_iterator
+		// * recursive_iterator, recursive_file_iterator, recursive_folder_iterator
 		Core::IO::Directory::Info::recursive_iterator end = info.recursive_end();
 		for (Core::IO::Directory::Info::recursive_iterator it = info.recursive_begin(); it != end; ++it)
 		{
+			// Check if it is a folder. isFile() also exists.
 			if (it.isFolder())
 				std::cout << "[D] " << it.filename() << std::endl;
 			else
-				std::cout << "    " << it.filename() << "  (" << it.size() << " bytes)" << std::endl;
+				// For files, display the size (in bytes)
+				std::cout << "    " << it.filename()
+						  << "  (" << it.size() << " bytes)" << std::endl;
 		}
 	}
 	return 0;
