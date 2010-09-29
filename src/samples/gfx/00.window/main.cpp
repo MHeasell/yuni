ab23433db2b0ba61a9aa1818ec3a7fa3553f317c
loomchild <loomchild@7e728022-c0ab-4603-8e65-c666ead28a33>
2010-09-29 12:42:14 +0000

    * Cmake coding style.  * Added gfx sample.
    git-svn-id: http://svn.ta3d.org/yuni/trunk@1273 7e728022-c0ab-4603-8e65-c666ead28a33

---
+++ src/samples/gfx/00.window/main.cpp
@@ -0,0 +1,29 @@
+
+#include <yuni/yuni.h>
+#include <yuni/application/gfx3d.h>
+#include <yuni/gfx/device.h>
+
+
+using namespace Yuni;
+
+
+class SampleApp : public Application::Gfx3D
+{
+public:
+	SampleApp(int argc, char* argv[])
+		: Application::Gfx3D(argc, argv)
+	{
+		Gfx::Engine::Ptr engine = Gfx::Engine::Instance();
+		Gfx::Device::Ptr dev = engine->device();
+		dev->type(Gfx::Device::Cairo);
+		engine->applicationTitle("Sample application");
+		engine->reset(dev);
+	}
+};
+
+int main(int argc, char* argv[])
+{
+	SampleApp app(argc, argv);
+	app.execute();
+	return app.exitCode();
+}
