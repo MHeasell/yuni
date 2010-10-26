927a07c75b2fea6aab6279b58a71bdc9bfbef527
milipili <milipili@shikami.org>
2010-10-26 21:23:49 +0000

    missing file
    git-svn-id: file:///home/michael/svn/yuni/trunk@1333 7e728022-c0ab-4603-8e65-c666ead28a33

---
+++ src/yuni/ui/fwd.h
@@ -0,0 +1,20 @@
+#ifndef __YUNI_UI_FWD_H__
+# define __YUNI_UI_FWD_H__
+
+
+namespace Yuni
+{
+namespace UI
+{
+
+	//! Forward declaration
+	template<class ChildT> class IQueueService;
+	class IControlContainer;
+	class Application;
+
+
+} // namespace UI
+} // namespace Yuni
+
+
+#endif // __YUNI_UI_FWD_H__
