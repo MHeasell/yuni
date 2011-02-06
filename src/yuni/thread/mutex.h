66032e2aea7ea4cc0f2230884947bbf18b38130c
milipili <milipili@shikami.org>
2011-02-06 15:41:39 +0000

    Mutex: Updated the documentation
    git-svn-id: http://svn.ta3d.org/yuni/trunk@1462 7e728022-c0ab-4603-8e65-c666ead28a33

--- src/yuni/thread/mutex.h
+++ src/yuni/thread/mutex.h
@@ -83,10 +83,11 @@ namespace Yuni
 
 
 	/*!
-	** \brief Locks a mutex in the constructor and unlocks it in the destructor.
+	** \brief Locks a mutex in the constructor and unlocks it in the destructor (RAII).
 	**
 	** This class is especially usefull for `get` accessor` and/or returned values
 	** which have to be thread-safe.
+	** This is a very common C++ idiom, known as "Resource Acquisition Is Initialization" (RAII).
 	**
 	** \code
 	**	  class Foo
