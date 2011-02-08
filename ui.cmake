7e5f53d1365279e3f25d2e7467454b1b49ca9f39
milipili <milipili@shikami.org>
2011-02-08 20:33:59 +0000

    Branch: keep ui.cmake as well
    git-svn-id: file:///home/michael/svn/yuni/branches/ui-wip@1466 7e728022-c0ab-4603-8e65-c666ead28a33

---
+++ ui.cmake
@@ -0,0 +1,115 @@
+
+YMESSAGE(":: [Module] UI  (EXPERIMENTAL, FOR ADVANCED USERS ONLY)")
+
+LIBYUNI_CONFIG_LIB("both" "ui"      "yuni-static-ui-core")
+
+LIBYUNI_CONFIG_DEPENDENCY("ui" "core") # core is required
+
+add_definitions("-DYUNI_MODULE_UI")
+
+
+list(APPEND SRC_UI
+
+	ui/application.h
+	ui/application.hxx
+	ui/application.cpp
+	ui/component.h
+	ui/component.hxx
+	ui/component.cpp
+	ui/control.h
+	ui/control.hxx ui/control.cpp
+	ui/controlcontainer.h
+	ui/controlcontainer.hxx
+	ui/controlcontainer.cpp
+	ui/desktop.h
+	ui/desktop.hxx
+	ui/desktop.cpp
+	ui/id.h
+	ui/id.cpp
+	ui/queueservice.h
+	ui/queueservice.hxx
+	ui/queueservice.cpp
+	ui/local-events.h
+	ui/local-events.cpp
+	ui/ui.h
+	ui/fwd.h
+
+	# Window Systems
+	ui/windowsystem.h
+	ui/windowsystem/windows.h
+	ui/windowsystem/x11.h
+	ui/windowsystem/cocoa.h
+	ui/windowsystem/native.h
+
+	# Local representations
+	ui/local/window.h ui/local/window.hxx ui/local/window.cpp
+	ui/local/windows/wingdiwindow.h
+	ui/local/windows/cairowindow.h ui/local/windows/cairowindow.hxx
+	ui/local/x11/x11yuni.h
+	ui/local/x11/x11window.h
+	ui/local/x11/cairowindow.h ui/local/x11/cairowindow.hxx
+
+	# Window
+	ui/window.h ui/window.hxx ui/window.cpp
+
+	# Standard Components
+	ui/button.h ui/button.hxx ui/button.cpp
+)
+
+
+#
+# Cairo
+#
+DEVPACK_IMPORT_CAIROPANGO()
+if (CAIRO_FOUND)
+	# We Have Cairo
+	YMESSAGE("Added Support for Cairo")
+	add_definitions("-DYUNI_USE_CAIRO")
+	target_link_libraries("${YUNI_EXT_CAIROPANGO_LIB}")
+	list(APPEND SRC_UI ui/local/surface/cairo.h ui/local/surface/cairo.cpp)
+
+	if (WIN32 OR WIN64)
+		# Cairo under Windows
+		list(APPEND SRC_UI ui/local/windows/cairowindow.cpp)
+	else(WIN32 OR WIN64)
+		if (UNIX AND NOT APPLE)
+			list(APPEND SRC_UI ui/local/x11/cairowindow.cpp)
+		endif (UNIX AND NOT APPLE)
+	endif (WIN32 OR WIN64)
+
+	# yuni-config
+	LIBYUNI_CONFIG_DEFINITION("both" "ui"  "YUNI_USE_CAIRO")
+	LIBYUNI_CONFIG_INCLUDE_PATH("both" "ui"  "${YUNI_EXT_CAIROPANGO_INCLUDE}")
+	LIBYUNI_CONFIG_LIB_RAW_COMMAND("both" "ui"  "${YUNI_EXT_CAIROPANGO_LIB}")
+
+else (CAIRO_FOUND)
+	YERROR("[!!] Impossible to find Cairo")
+endif (CAIRO_FOUND)
+
+
+#
+# Pango
+#
+if (PANGO_FOUND)
+	YMESSAGE("Added Support for Pango")
+	add_definitions("-DYUNI_USE_PANGO")
+	LIBYUNI_CONFIG_DEFINITION("both" "ui"  "YUNI_USE_PANGO")
+else (PANGO_FOUND)
+	YERROR("[!!] Impossible to find Pango")
+endif (PANGO_FOUND)
+
+
+#
+# Platform-dependent
+#
+if (WIN32 OR WIN64)
+	list(APPEND SRC_UI ui/local/windows/wingdiwindow.cpp ui/windowsystem/windows.cpp)
+else()
+	if (UNIX AND NOT APPLE)
+		list(APPEND SRC_UI ui/local/x11/x11window.cpp ui/windowsystem/x11.cpp)
+	endif ()
+endif ()
+
+
+add_library(yuni-static-ui-core STATIC ${SRC_UI})
+
