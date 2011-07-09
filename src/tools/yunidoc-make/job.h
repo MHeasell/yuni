6c0e779d31af3bcf0702d80e0995c9113a0dca8d
milipili <milipili@shikami.org>
2011-07-09 21:40:25 +0000

    Doc
    git-svn-id: file:///home/michael/svn/yuni/trunk@1597 7e728022-c0ab-4603-8e65-c666ead28a33

--- src/tools/yunidoc-make/job.h
+++ src/tools/yunidoc-make/job.h
@@ -12,19 +12,29 @@
 class CompileJob : public Yuni::Job::IJob
 {
 public:
+	//! \name Constructor & Destructor
+	//@{
+	/*!
+	** \brief Constructor
+	*/
 	CompileJob(const Yuni::String& input, const Yuni::String& htdocs);
+	//! Destructor
 	virtual ~CompileJob();
+	//@}
 
 	void add(const Yuni::String& filename) {pSources.push_back(filename);}
 
 private:
 	virtual void onExecute();
 
+	void generate(const Yuni::String& rel, const Yuni::String& src, const Yuni::String& target);
+
 private:
 	const Yuni::String& pInput;
 	const Yuni::String& pHtdocs;
 	Yuni::String::Vector  pSources;
-};
+
+}; // class CompileJob
 
 
 extern Yuni::Job::QueueService<>  queueService;
