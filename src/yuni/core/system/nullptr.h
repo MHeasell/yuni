b20d945ba2b2338581850365647f8ee5cd05b1c1
loomchild <loomchild@7e728022-c0ab-4603-8e65-c666ead28a33>
2011-09-20 21:02:57 +0000

    Moved nullptr to :: namespace. Fixes #370.
    git-svn-id: http://svn.ta3d.org/yuni/trunk@1811 7e728022-c0ab-4603-8e65-c666ead28a33

--- src/yuni/core/system/nullptr.h
+++ src/yuni/core/system/nullptr.h
@@ -8,62 +8,62 @@
 # endif
 
 
+# ifdef YUNI_HAS_NULLPTR
+
+typedef std::nullptr_t YuniNullPtr;
+
+# else
+
+/*!
+** \brief C++ Idioms/nullptr
+**
+** \see proposal (N2431)
+*/
+const class YuniNullPtr
+{
+public:
+	/*! Convertible to any type of null non-member */
+	template<class T> inline operator T*() const
+	{
+		return 0;
+	}
+
+	/*!
+	** \brief Any type of null member pointer
+	**
+	** \note In gcc 4.1.1 compiler that does not recognize the comparison of
+	** nullptr with point to member function.
+	*/
+	template<class C, class T> inline operator T C::*() const
+	{
+		return 0;
+	}
+
+# if YUNI_OS_GCC_VERSION >= 40400
+	template<class T> bool operator == (const T* rhs) const
+	{
+		return !rhs;
+	}
+
+	template<class T> bool operator != (const T* rhs) const
+	{
+		return (NULL != rhs);
+	}
+# endif
+
+private:
+	void operator&() const;  /* Can't take address of nullptr */
+
+} nullptr = {};
+
+
+# endif // nullptr support
+
 
 namespace Yuni
 {
 
-	# ifdef YUNI_HAS_NULLPTR
-
-	typedef std::nullptr_t NullPtr;
-
-	# else
-
-	/*!
-	** \brief C++ Idioms/nullptr
-	**
-	** \see proposal (N2431)
-	*/
-	const class NullPtr
-	{
-	public:
-		/*! Convertible to any type of null non-member */
-		template<class T> inline operator T*() const
-		{
-			return 0;
-		}
-
-		/*!
-		** \brief Any type of null member pointer
-		**
-		** \note In gcc 4.1.1 compiler that does not recognize the comparison of
-		** nullptr with point to member function.
-		*/
-		template<class C, class T> inline operator T C::*() const
-		{
-			return 0;
-		}
-
-		# if YUNI_OS_GCC_VERSION >= 40400
-		template<class T> bool operator == (const T* rhs) const
-		{
-			return !rhs;
-		}
-
-		template<class T> bool operator != (const T* rhs) const
-		{
-			return (NULL != rhs);
-		}
-		# endif
-
-	private:
-		void operator&() const;  /* Can't take address of nullptr */
-
-	} nullptr = {};
-
-
-	# endif // nullptr support
-
-
+	typedef YuniNullPtr  NullPtr;
 
 } /* namespace Yuni */
 
