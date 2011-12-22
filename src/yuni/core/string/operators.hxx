b4b8abf94c89c0047000ea9dad593fca00f9ea71
milipili <milipili@shikami.org>
2011-12-22 22:33:18 +0000

    std::ostream: added overload for std::list and std::vector (requires include core/string.h)
    git-svn-id: http://svn.ta3d.org/yuni/trunk@1921 7e728022-c0ab-4603-8e65-c666ead28a33

--- src/yuni/core/string/operators.hxx
+++ src/yuni/core/string/operators.hxx
@@ -21,6 +21,58 @@ inline std::ostream& operator << (std::ostream& out, const Yuni::UTF8::Char& rhs
 }
 
 
+template<class ContainerT>
+class OStreamYuniHelper
+{
+public:
+	static void Print(std::ostream& out, const ContainerT& rhs)
+	{
+		switch (rhs.size())
+		{
+			case 0:
+				break;
+			case 1:
+				{
+					out << rhs[0];
+					break;
+				}
+			default:
+				{
+					const Yuni::String::Vector::const_iterator end = rhs.end();
+					Yuni::String::Vector::const_iterator i = rhs.begin();
+					out << *i;
+					++i;
+					for (; i != end; ++i)
+					{
+						out.write(", ", 2);
+						out << *i;
+					}
+					break;
+				}
+		}
+	}
+
+}; // class OStreamYuniHelper
+
+
+
+template<class T, class AllocatorT>
+std::ostream& operator << (std::ostream& out, const std::vector<T, AllocatorT>& rhs)
+{
+	typedef std::vector<T, AllocatorT>  SourceType;
+	OStreamYuniHelper<SourceType>::Print(out, rhs);
+	return out;
+}
+
+
+template<class T, class AllocatorT>
+std::ostream& operator << (std::ostream& out, const std::list<T, AllocatorT>& rhs)
+{
+	typedef std::list<T, AllocatorT>  SourceType;
+	OStreamYuniHelper<SourceType>::Print(out, rhs);
+	return out;
+}
+
 
 
 
