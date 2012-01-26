712d7821f0c70743552659495b253caa7ff51d60
loomchild <loomchild@7e728022-c0ab-4603-8e65-c666ead28a33>
2012-01-26 17:19:12 +0000

    Audio : Rearchitectured the sample, plus some minor changes.
    git-svn-id: file:///home/michael/svn/yuni/trunk@1976 7e728022-c0ab-4603-8e65-c666ead28a33

--- src/samples/audio/00.loadandplay/00.loadandplay.cpp
+++ src/samples/audio/00.loadandplay/00.loadandplay.cpp
@@ -1,3 +1,10 @@
+/*!
+** This sample demonstrates basic audio playing abilities.
+** It takes file names as command line arguments and plays their audio streams one after the other.
+** To demonstrate that the main loop is free to do something else, the sample displays elapsed time
+** for the first file.
+** This sample accepts audio streams from either audio or video files indifferently.
+*/
 
 #include <vector>
 #include <yuni/yuni.h>
@@ -8,70 +15,73 @@
 using namespace Yuni;
 
 
-class LoadAndPlay
+// Display the time in seconds using an MM:SS format
+template<typename T>
+static void writeTime(T timeInSeconds)
 {
-public:
-	LoadAndPlay(int argc, char* argv[])
+	std::cout << (timeInSeconds / 60) << ":";
+	if (timeInSeconds % 60 < 10)
+		std::cout << 0;
+	std::cout << (timeInSeconds % 60);
+}
+
+
+
+int main(int argc, char* argv[])
+{
+	if (argc < 2)
 	{
-		for (int i = 1; i < argc; ++i)
-		{
-			pFilenames.push_back(argv[i]);
-		}
-		if (!pFilenames.empty())
-			audio.start();
+		std::cerr << "Usage: " << argv[0] << " <file1> [ <file2>, ... ]" << std::endl;
+		return 1;
 	}
 
-	~LoadAndPlay()
+	// The audio queue service is the center point for audio manipulation
+	Audio::QueueService audio;
+	audio.start();
+
+	if (!audio.running())
+		return 1;
+
+	String emitterName;
+	for (int i = 1; i < argc; ++i)
 	{
-		if (!pFilenames.empty())
-			audio.stop();
-	}
-
-	void onExecute()
-	{
-		if (!audio.running())
-			return;
-
-		String emitterName;
-
-		uint i = 0;
-		const String::Vector::const_iterator end = pFilenames.end();
-		for (String::Vector::const_iterator it = pFilenames.begin(); it != end; ++it)
+		// Load sound file
+		if (!audio.bank.load(argv[i]))
 		{
-			// Load sound file
-			if (!audio.bank.load(*it))
-				return;
-
-			// Create emitter
-			emitterName.clear() << "Emitter " << i++;
-			if (!audio.emitter.add(emitterName))
-			{
-				std::cerr << "Emitter creation failed !" << std::endl;
-				return;
-			}
-
-			// Attach the emitter to the buffer
-			audio.emitter.attach(emitterName, *it);
-			// Start playback on the emitter
-			audio.emitter.play(emitterName);
-
-			Yuni::SuspendMilliSeconds(1000);
-			// Get stream duration
-			unsigned int duration = audio.bank.duration(*it);
-			std::cout << "Sound duration: ";
-			writeTime(duration);
-			std::cout << std::endl;
-
+			std::cerr << "Failed to load \"" << argv[i] << "\"" << std::endl;
+			return 1;
 		}
+
+		// Generate a unique name for the emitter ...
+		emitterName.clear() << "Emitter " << i;
+		// ... and create it
+		if (!audio.emitter.add(emitterName))
+		{
+			std::cerr << "Emitter creation failed !" << std::endl;
+			return 1;
+		}
+
+		// Attach the emitter to the buffer
+		audio.emitter.attach(emitterName, argv[i]);
+		// Start playback on the emitter
+		audio.emitter.play(emitterName);
+
+		Yuni::SuspendMilliSeconds(1000);
+		// Get stream duration
+		unsigned int duration = audio.bank.duration(argv[i]);
+		std::cout << "Sound duration: ";
+		writeTime(duration);
+		std::cout << std::endl;
+
 		sint64 elapsed = 0;
-		// Play for 3000 * 100ms = 300s = 5min
-		for (unsigned int i = 0; i < 3000; ++i)
+		// Keep playing while at least one emitter is playing
+		while (audio.playing())
 		{
 			// Every 100ms
 			Yuni::SuspendMilliSeconds(100);
 
 			// Get elapsed playback time
-			sint64 newTime = audio.emitter.elapsedTime("Emitter 0");
+			sint64 newTime = audio.emitter.elapsedTime(emitterName);
 			// Only update if different
 			if (newTime != elapsed)
 			{
@@ -84,34 +94,8 @@ public:
 		}
 	}
 
-private:
-
-	// Display the time in seconds using an MM:SS format
-	template<typename T>
-	void writeTime(T timeInSeconds)
-	{
-		std::cout << (timeInSeconds / 60) << ":";
-		if (timeInSeconds % 60 < 10)
-			std::cout << 0;
-		std::cout << (timeInSeconds % 60);
-	}
-
-
-private:
-	// Store the file names given on the command line
-	String::Vector pFilenames;
-	// The audio queue service is the center point for audio manipulation
-	Audio::QueueService audio;
-
-}; // class LoadAndPlay
-
-
-
-
-int main(int argc, char* argv[])
-{
-	LoadAndPlay app(argc, argv);
-	app.onExecute();
+	// Properly stop the queue service and unload everything
+	audio.stop();
 	return 0;
 }
 
