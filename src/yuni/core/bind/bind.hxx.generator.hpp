cd18060e8adcb5ea14dc96c5a5b4e8d0a5b867ce
loomchild <loomchild@7e728022-c0ab-4603-8e65-c666ead28a33>
2012-01-31 23:09:04 +0000

    Bind : proto fix.
    git-svn-id: file:///home/michael/svn/yuni/trunk@1990 7e728022-c0ab-4603-8e65-c666ead28a33

--- src/yuni/core/bind/bind.hxx.generator.hpp
+++ src/yuni/core/bind/bind.hxx.generator.hpp
@@ -141,9 +141,20 @@ namespace Yuni
 		template <class> class OwspP, template <class> class ChckP, class ConvP,
 		template <class> class StorP, template <class> class ConsP>
 	inline void
-	Bind<<%=tmpl[1]%>, <%=tmpl[2]%>>::bind(const SmartPtr<C, OwspP,ChckP,ConvP,StorP,ConsP> c, R (C::*member)(<%=generator.list(i)%>))
+	Bind<<%=tmpl[1]%>, <%=tmpl[2]%>>::bind(const SmartPtr<C, OwspP,ChckP,ConvP,StorP,ConsP>& c, R (C::*member)(<%=generator.list(i)%>))
 	{
-		bind(SmartPtr<C, OwspP,ChckP,ConvP,StorP,ConsP>::WeakPointer(c), member);
+		if (!(!c))
+		{
+			// The object is not null, go ahead
+			// Assigning a new bind
+			typedef R (C::*MemberType)(<%=generator.list(i)%>);
+			typedef SmartPtr<C, OwspP,ChckP,ConvP,StorP,ConsP> PtrT;
+			pHolder = new Private::BindImpl::BoundWithSmartPtrMember<PtrT, R(<%=generator.list(i)%>)>
+				(c, reinterpret_cast<MemberType>(member));
+		}
+		else
+			// The object is null, we should unbind
+			unbind();
 	}
 
 
@@ -153,9 +164,20 @@ namespace Yuni
 		template <class> class OwspP, template <class> class ChckP, class ConvP,
 		template <class> class StorP, template <class> class ConsP>
 	inline void
-	Bind<<%=tmpl[1]%>, <%=tmpl[2]%>>::bind(const SmartPtr<C, OwspP,ChckP,ConvP,StorP,ConsP> c, R (C::*member)(<%=generator.list(i)%>) const)
+	Bind<<%=tmpl[1]%>, <%=tmpl[2]%>>::bind(const SmartPtr<C, OwspP,ChckP,ConvP,StorP,ConsP>& c, R (C::*member)(<%=generator.list(i)%>) const)
 	{
-		bind(SmartPtr<C, OwspP,ChckP,ConvP,StorP,ConsP>::WeakPointer(c), member);
+		if (!(!c))
+		{
+			// The object is not null, go ahead
+			// Assigning a new bind
+			typedef R (C::*MemberType)(<%=generator.list(i)%>);
+			typedef SmartPtr<C, OwspP,ChckP,ConvP,StorP,ConsP> PtrT;
+			pHolder = new Private::BindImpl::BoundWithSmartPtrMember<PtrT, R(<%=generator.list(i)%>)>
+				(c, reinterpret_cast<MemberType>(member));
+		}
+		else
+			// The object is null, we should unbind
+			unbind();
 	}
 
 
@@ -165,10 +187,21 @@ namespace Yuni
 		template <class> class OwspP, template <class> class ChckP, class ConvP,
 		template <class> class StorP, template <class> class ConsP>
 	inline void
-	Bind<<%=tmpl[1]%>, <%=tmpl[2]%>>::bind(const SmartPtr<C, OwspP,ChckP,ConvP,StorP,ConsP> c, R (C::*member)(<%=generator.list(i,"A", "", ", ")%>U),
+	Bind<<%=tmpl[1]%>, <%=tmpl[2]%>>::bind(const SmartPtr<C, OwspP,ChckP,ConvP,StorP,ConsP>& c, R (C::*member)(<%=generator.list(i,"A", "", ", ")%>U),
 		typename Bind<<%=tmpl[1]%>, <%=tmpl[2]%>>::template WithUserData<U>::ParameterType userdata)
 	{
-		bind(SmartPtr<C, OwspP,ChckP,ConvP,StorP,ConsP>::WeakPointer(c), member, userdata);
+		if (!(!c))
+		{
+			// The object is not null, go ahead
+			// Assigning a new bind
+			typedef SmartPtr<C, OwspP,ChckP,ConvP,StorP,ConsP> PtrT;
+			pHolder = new Private::BindImpl::BoundWithSmartPtrMemberAndUserData<
+				typename WithUserData<U>::ParameterType, PtrT, R(<%=generator.list(i,"A","",", ")%>U)>
+				(c, member, userdata);
+		}
+		else
+			// The object is null, we should unbind
+			unbind();
 	}
 
 
@@ -178,10 +211,21 @@ namespace Yuni
 		template <class> class OwspP, template <class> class ChckP, class ConvP,
 		template <class> class StorP, template <class> class ConsP>
 	inline void
-	Bind<<%=tmpl[1]%>, <%=tmpl[2]%>>::bind(const SmartPtr<C, OwspP,ChckP,ConvP,StorP,ConsP> c, R (C::*member)(<%=generator.list(i,"A", "", ", ")%>U) const,
+	Bind<<%=tmpl[1]%>, <%=tmpl[2]%>>::bind(const SmartPtr<C, OwspP,ChckP,ConvP,StorP,ConsP>& c, R (C::*member)(<%=generator.list(i,"A", "", ", ")%>U) const,
 		typename Bind<<%=tmpl[1]%>, <%=tmpl[2]%>>::template WithUserData<U>::ParameterType userdata)
 	{
-		bind(SmartPtr<C, OwspP,ChckP,ConvP,StorP,ConsP>::WeakPointer(c), member, userdata);
+		if (!(!c))
+		{
+			// The object is not null, go ahead
+			// Assigning a new bind
+			typedef SmartPtr<C, OwspP,ChckP,ConvP,StorP,ConsP> PtrT;
+			pHolder = new Private::BindImpl::BoundWithSmartPtrMemberAndUserData<
+				typename WithUserData<U>::ParameterType, PtrT, R(<%=generator.list(i,"A","",", ")%>U)>
+				(c, member, userdata);
+		}
+		else
+			// The object is null, we should unbind
+			unbind();
 	}
 
 
