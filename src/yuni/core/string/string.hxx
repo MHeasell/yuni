9a786fe1b890b74410454f70b00c8ffa5fda6bc8
milipili <milipili@shikami.org>
2012-07-23 08:36:04 +0000

    Compilation fix
    git-svn-id: file:///home/michael/svn/yuni/trunk@2120 7e728022-c0ab-4603-8e65-c666ead28a33

--- src/yuni/core/string/string.hxx
+++ src/yuni/core/string/string.hxx
@@ -2452,7 +2452,7 @@ namespace Yuni
 	inline char*
 	CString<ChunkSizeT,ExpandableT,ZeroTerminatedT>::data()
 	{
-		return AncestorType::size ? AncestorData::data : "";
+		return AncestorType::size ? AncestorType::data : "";
 	}
 
 
@@ -2460,7 +2460,7 @@ namespace Yuni
 	inline const char*
 	CString<ChunkSizeT,ExpandableT,ZeroTerminatedT>::c_str() const
 	{
-		return AncestorType::size ? AncestorData::data : "";
+		return AncestorType::size ? AncestorType::data : "";
 	}
 
 
