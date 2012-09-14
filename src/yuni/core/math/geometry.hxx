2e30034b15af5a67e61aac89a0c3f658c3e82272
loomchild <loomchild@7e728022-c0ab-4603-8e65-c666ead28a33>
2012-09-14 11:38:14 +0000

    * Additions in geometry and trigonometry. * Compile fix for MinGW : process.cpp
    git-svn-id: http://svn.ta3d.org/yuni/trunk@2146 7e728022-c0ab-4603-8e65-c666ead28a33

--- src/yuni/core/math/geometry.hxx
+++ src/yuni/core/math/geometry.hxx
@@ -8,8 +8,9 @@ namespace Geometry
 
 
 	template<typename T>
-	inline Point3D<T> Vector3D<T>::IntersectionLinePlane(const Point3D<T>& linePoint, const Vector3D<T>& lineDirection,
-		const Point3D<T>& planePoint, const Vector3D<T>& planeNormal)
+	inline Point3D<T> LinePlaneIntersection(const Point3D<T>& linePoint,
+		const Vector3D<T>& lineDirection, const Point3D<T>& planePoint,
+		const Vector3D<T>& planeNormal)
 	{
 		float dotProduct = DotProduct(planeNormal, lineDirection);
 		assert(Math::Abs(dotProduct) < YUNI_EPSILON);
@@ -23,6 +24,42 @@ namespace Geometry
 		Point3D<T> result(linePoint);
 		result.translate(direction.x, direction.y, direction.z);
 		return result;
+	}
+
+
+	template<typename T>
+	inline Point3D<T> PointToLineProjection(const Point3D<T>& point,
+		const Point3D<T>& linePoint, const Vector3D<T>& lineDirection)
+	{
+		Vector3D<> translation(lineDirection);
+		translation.normalize();
+		float dot = Vector3D<>::DotProduct(translation, Vector3D<>(linePoint, point));
+		translation *= dot;
+		Point3D<> projection(linePoint);
+		projection.translate(translation.x, translation.y, translation.z);
+		return projection;
+	}
+
+
+	template<typename T>
+	inline Point3D<T> PointToPlaneProjection(const Point3D<T>& point,
+		const Point3D<T>& planePoint, const Vector3D<T>& planeNormal)
+	{
+		// First project on a normal line to the plane
+		Point3D<> pn = PointToLineProjection(point, planePoint, planeNormal);
+		// Get the vector from the projection to the point
+		Vector3D<> translation(pn, point);
+		// Translate the plane origin by this vector
+		Point3D<> projection(planePoint);
+		projection.translate(translation.x, translation.y, translation.z);
+		return projection;
+	}
+
+
+	template<typename T>
+	inline T VectorAngle(const Vector3D<T>& start, const Vector3D<T>& destination)
+	{
+		return 0.0f;
 	}
 
 
