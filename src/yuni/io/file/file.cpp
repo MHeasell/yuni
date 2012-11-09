7440144abc0010a2d2be83266627999d8053a7a8
milipili <milipili@shikami.org>
2012-11-09 10:33:39 +0000

    IO::File::Size: fixed invalid path normalization
    git-svn-id: http://svn.ta3d.org/yuni/trunk@2206 7e728022-c0ab-4603-8e65-c666ead28a33

--- src/yuni/io/file/file.cpp
+++ src/yuni/io/file/file.cpp
@@ -49,7 +49,7 @@ namespace File
 		}
 
 		String  norm;
-		Yuni::IO::Normalize(norm, p, len);
+		Yuni::IO::Normalize(norm, AnyString(p, len));
 		// Conversion into wchar_t
 		Private::WString<true> wstr(norm);
 		if (wstr.empty())
