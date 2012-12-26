fecf100210890a429e8f951e14020b9445398f8a
milipili <milipili@shikami.org>
2012-12-26 00:51:16 +0000

    sample: started a new example for Net::Messaging::Service
    git-svn-id: file:///home/michael/svn/yuni/trunk@2284 7e728022-c0ab-4603-8e65-c666ead28a33

---
+++ src/samples/net/00.simple.rest.server/main.cpp
@@ -0,0 +1,90 @@
+
+#include <yuni/yuni.h>
+#include <yuni/net/messaging/service.h>
+#include <yuni/core/logs.h>
+
+using namespace Yuni;
+
+//! Logging facility
+Logs::Logger<> logs;
+
+
+
+static void PrepareTheAPI(Net::Messaging::Service& service)
+{
+	logs.info() << "preparing protocol";
+	// note: contrary to the object `service`, `myapi` is not thread-safe
+	Net::Messaging::Protocol* myapi = new Net::Messaging::Protocol();
+
+	// method: hello_world
+	myapi->methods.add("hello_world")
+		.brief("Always answer 'hello world to any request'")
+		.option("http.method", "GET");
+
+	// Switching to the new protocol
+	//
+	// note: The method `protocol()` must be call to make any modification to
+	//   the protocol visible, and can be called anytime, even if started
+	//   (useful for reloading the service while running)
+	// note: this method will take ownership of the pointer, thus you no
+	//   longer be used
+	service.protocol(myapi);
+}
+
+
+static void PrepareTransports(Net::Messaging::Service& service)
+{
+	logs.info() << "preparing transports";
+	//service.transports += new Net::Messaging::Transport::REST::Server();
+}
+
+
+static bool StartService(Net::Messaging::Service& service)
+{
+	logs.info() << "starting";
+
+	Net::Error error = service.start();
+
+	switch (error)
+	{
+		case Net::errNone:
+			break;
+		default:
+			logs.fatal() << "impossible to start the service: " << error;
+			logs.info() << "aborting";
+			return false;
+	}
+
+	logs.info(); // empty line for beauty
+	return true;
+}
+
+
+
+
+
+int main(int, char**)
+{
+	// Welcome !
+	logs.notice() << "Yuni REST Server Example";
+	logs.info() << "http://www.libyuni.org";
+	logs.info(); // empty line
+
+	// The service itself
+	Net::Messaging::Service  service;
+
+	// Prepare the API
+	PrepareTheAPI(service);
+
+	// Preparing all transports
+	PrepareTransports(service);
+
+	// Starting the new service
+	if (not StartService(service))
+		return EXIT_FAILURE;
+
+	// Waiting for the end of the server
+	service.wait();
+	return 0;
+}
+
