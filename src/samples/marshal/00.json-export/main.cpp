5e05244fbc7e9b0843b38df6c5e8c4f0d8da7cd0
milipili <milipili@shikami.org>
2013-01-03 16:28:32 +0000

    Sample: Marshal Object: added comments
    git-svn-id: file:///home/michael/svn/yuni/trunk@2319 7e728022-c0ab-4603-8e65-c666ead28a33

--- src/samples/marshal/00.json-export/main.cpp
+++ src/samples/marshal/00.json-export/main.cpp
@@ -33,6 +33,24 @@ int main(int, char**)
 	// Exporting to JSON
 	Clob out;
 	root.toJSON(out);
+
+	// The output should look like the following :
+	// {
+	//    "movies": {
+	//        "Disney": {
+	//            "Brave": "http://www.imdb.com/title/tt1217209/",
+	//            "Wreck-It Ralph": "http://www.imdb.com/title/tt1772341/"
+	//        },
+	//        "Dreamworks": {
+	//            "Dragons: Riders of Berk": "http://www.imdb.com/title/tt2325846/",
+	//            "Rise of the Guardians": "http://www.imdb.fr/title/tt1446192/"
+	//        },
+	//        "Pixar": {
+	//            "Cars": "http://www.imdb.com/title/tt0317219/",
+	//            "Cars 2": "http://www.imdb.com/title/tt1216475/"
+	//        }
+	//    }
+	// }
 	std::cout << out << std::endl;
 	return 0;
 }
