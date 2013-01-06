7b067c5c94228dfc850368d298efabadc56685b6
milipili <milipili@shikami.org>
2013-01-06 00:26:57 +0000

    Marshal: added JSON parser (imported from Nany)
    git-svn-id: http://svn.ta3d.org/yuni/trunk@2328 7e728022-c0ab-4603-8e65-c666ead28a33

--- src/yuni/cmake/marshal.cmake
+++ src/yuni/cmake/marshal.cmake
@@ -4,6 +4,24 @@ YMESSAGE_MODULE("Marshal (Object serialization)  (EXPERIMENTAL)")
 LIBYUNI_CONFIG_LIB("both" "marshal"        "yuni-static-marshal")
 
 
+# Sources for parsing JSON are imported from nany and no relative paths
+# is currently provided
+include_directories("..")
+
+set (SRC_PRIVATE_JSON
+	private/marshal/json/grammar.cpp
+	private/marshal/json/grammar.h
+	private/marshal/json/grammar.hxx
+	private/marshal/json/internal-charsets.hxx
+	private/marshal/json/internal-dfa.hxx
+	private/marshal/json/internal-lalr.hxx
+	private/marshal/json/internal-productions.hxx
+	private/marshal/json/internal-symbols.hxx
+	private/marshal/json/internal-table.hxx
+	private/marshal/json/symbols.h
+)
+source_group("Marshal\\Private\\JSON" FILES ${SRC_PRIVATE_JSON})
+
 set(SRC_MARSHAL
 	marshal/fwd.h
 	marshal/document.h
@@ -18,6 +36,7 @@ source_group("Marshal" FILES ${SRC_MARSHAL})
 
 add_Library(yuni-static-marshal STATIC
 	${SRC_MARSHAL}
+	${SRC_PRIVATE_JSON}
 )
 
 # Setting output path
