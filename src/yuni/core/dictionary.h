0f56a9db2d13e24d45ec7b1dc0304d51f1fbe33e
loomchild <loomchild@7e728022-c0ab-4603-8e65-c666ead28a33>
2013-06-25 21:29:26 +0000

    Added typedefs for Hash/map dictionaries
    git-svn-id: http://svn.ta3d.org/yuni/trunk@2444 7e728022-c0ab-4603-8e65-c666ead28a33

--- src/yuni/core/dictionary.h
+++ src/yuni/core/dictionary.h
@@ -21,13 +21,20 @@ namespace Yuni
 	class Dictionary final
 	{
 	public:
-		//! Un unordered dictionary table
 		# ifndef YUNI_HAS_STL_UNORDERED_MAP
+		//! Un unordered dictionary table
 		typedef std::map<KeyT, ValueT> Hash;
+		//! Un unordered dictionary table
+		typedef std::map<KeyT, ValueT> Unordered;
 		# else
+		//! Un unordered dictionary table
 		typedef std::unordered_map<KeyT, ValueT> Hash;
+		//! Un unordered dictionary table
+		typedef std::unordered_map<KeyT, ValueT> Unordered;
 		# endif
 
+		//! Un ordered dictionary table
+		typedef std::map<KeyT, ValueT> Map;
 		//! Un ordered dictionary table
 		typedef std::map<KeyT, ValueT> Ordered;
 
@@ -39,13 +46,20 @@ namespace Yuni
 	class Set final
 	{
 	public:
-		//! Un unordered set table
 		# ifndef YUNI_HAS_STL_UNORDERED_MAP
+		//! Un unordered set table
 		typedef std::set<KeyT> Hash;
+		//! Un unordered set table
+		typedef std::set<KeyT> Unordered;
 		# else
+		//! Un unordered set table
 		typedef std::unordered_set<KeyT> Hash;
+		//! Un unordered set table
+		typedef std::unordered_set<KeyT> Unordered;
 		# endif
 
+		//! Un ordered dictionary table
+		typedef std::set<KeyT> Set;
 		//! Un ordered dictionary table
 		typedef std::set<KeyT> Ordered;
 
